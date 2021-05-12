6d1e2d43fbc382dc596f492797fcda335fd61d0d
hsolbrig <solbrig@jhu.edu>
2021-05-12 14:57:56 -0500

    Add test for issue #3
    Fixed some issues in the tox file

---
+++ tests/linkml_runtime/test_issues/test_include_schema.py
@@ -0,0 +1,17 @@
+import unittest
+
+from linkml_runtime.linkml_model.meta import SchemaDefinition
+from linkml_runtime.loaders import yaml_loader
+from tests.test_issues.environment import env
+
+class IncludeSchemaTestCase(unittest.TestCase):
+    """ include_schema.yaml produces a Python exception on an uncaught error"""
+    # "Awaiting fix for issue #3"
+    @unittest.expectedFailure
+    def test_include_schema(self):
+        inp = yaml_loader.load(env.input_path('include_schema.yaml'), SchemaDefinition)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
