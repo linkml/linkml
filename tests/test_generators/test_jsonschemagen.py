2f3c10c4de1d2bb1f4712a239949eaf9ddb56d5c
cmungall <cjm@berkeleybop.org>
2021-06-25 19:46:34 -0700

    negative tests

--- tests/test_generators/test_jsonschemagen.py
+++ tests/test_generators/test_jsonschemagen.py
@@ -1,15 +1,14 @@
 import unittest
 
-from linkml_runtime.loaders.yaml_loader import YAMLLoader
 from linkml_runtime.loaders import yaml_loader, json_loader, rdf_loader
 from linkml_runtime.dumpers import json_dumper
 from linkml.generators.jsonschemagen import JsonSchemaGenerator
 from linkml.generators.pythongen import PythonGenerator
-from linkml_runtime.linkml_model.meta import LINKML
 from tests.test_generators.environment import env
 from importlib import import_module
 import jsonschema
 import json
+import yaml
 #from kitchen_sink import *
 from output.kitchen_sink import *
 
@@ -17,9 +16,14 @@ SCHEMA = env.input_path('kitchen_sink.yaml')
 JSONSCHEMA_OUT = env.expected_path('kitchen_sink.schema.json')
 PYTHON = env.expected_path('kitchen_sink.py')
 DATA = env.input_path('kitchen_sink_inst_01.yaml')
+FAILDATA = env.input_path('kitchen_sink_failtest_inst_01.yaml')
 DATA_JSON = env.expected_path('kitchen_sink_inst_01.json')
 
 def make_python() -> None:
+    """
+    Note: if you change the yaml schema and associated test instance objects,
+    you may need to run this test twice
+    """
     pstr = str(PythonGenerator(SCHEMA, mergeimports=True).serialize())
     #eval(compile(pstr))
     with open(PYTHON, 'w') as io:
@@ -41,6 +45,20 @@ class JsonSchemaTestCase(unittest.TestCase):
         jsonschema_obj = json.loads(jsonschemastr)
         jsonschema.validate(inst_dict, schema=jsonschema_obj)
         #print(f'S={jsonschema}')
+
+        with open(FAILDATA, 'r') as io:
+            failobjs = yaml.load(io)
+        for failobj in failobjs:
+            dataset = failobj['dataset']
+            print(f'Testing {failobj["description"]} {dataset}')
+            is_fail = False
+            try:
+                jsonschema.validate(dataset, schema=jsonschema_obj)
+                is_fail = False
+            except:
+                is_fail = True
+            assert is_fail
+
 
 
 
