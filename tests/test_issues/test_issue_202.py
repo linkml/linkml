556e8ccb06e9365ae4e940846e0ae4c1662a7a45
hsolbrig <solbrig@jhu.edu>
2021-07-05 15:36:54 -0500

    Add test case for issue #202
    See tests/test_issues-output/issue_202.json.schema for current output

---
+++ tests/test_issues/test_issue_202.py
@@ -0,0 +1,22 @@
+import unittest
+
+from jsonasobj2 import loads
+
+from linkml.generators.jsonschemagen import JsonSchemaGenerator
+from tests.test_issues.environment import env
+from tests.utils.test_environment import TestEnvironmentTestCase
+
+
+class Issue202TestCase(TestEnvironmentTestCase):
+    env = env
+
+    def test_issue_202(self):
+        json_str = env.generate_single_file('issue_202.json.schema',
+                                            lambda: JsonSchemaGenerator(env.input_path('issue_202.yaml')).serialize(),
+                                            value_is_returned=True)
+        json_schema = loads(json_str)
+        self.assertEqual(json_schema.definitions.GeospatialDDCoordLocation.properties.latitude.type, "number")
+
+
+if __name__ == '__main__':
+    unittest.main()
