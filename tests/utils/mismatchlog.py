5ac1eb4aacc27cb0e793a3c432f5bf7b5dbc58d6
hsolbrig <solbrig@jhu.edu>
2021-07-09 10:34:55 -0500

    Change text of mismatch message to get "error" out of it
    Don't fail on test_shexgen because of wrong pyld package.

--- tests/utils/mismatchlog.py
+++ tests/utils/mismatchlog.py
@@ -37,7 +37,7 @@ class MismatchLog:
                 frame = frame.f_back
 
         def __str__(self):
-            rval = [f'Validation error: File "{os.path.relpath(self.file_or_directory, base_dir)}", line 1']
+            rval = [f'Output mismatch: File "{os.path.relpath(self.file_or_directory, base_dir)}", line 1']
             rval.append("Stack:  " + '\n\t\t'.join([str(e) for e in self.call_stack]))
             if self.msg:
                 rval.append(self.msg.rstrip().capitalize())
