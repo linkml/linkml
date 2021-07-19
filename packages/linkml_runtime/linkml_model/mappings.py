a732c5a74a311e2061cbc33c81ad5063a7e9c0f0
hsolbrig <solbrig@jhu.edu>
2021-07-19 14:42:05 -0500

    Update to copy of linkml-model 1.0.1

--- packages/linkml_runtime/linkml_model/mappings.py
+++ packages/linkml_runtime/linkml_model/mappings.py
@@ -1,5 +1,5 @@
 # Auto generated from mappings.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-06-11 08:45
+# Generation date: 2021-06-30 17:04
 # Schema: mappings
 #
 # id: https://w3id.org/linkml/mappings
