77f85414c96b8c23cd11b78e910d36ce3258898a
GitHub Action <action@github.com>
2021-08-14 01:44:50 +0000

    Automated adding outputs from tests

--- tests/test_scripts/output/genpython/testtypes.py
+++ tests/test_scripts/output/genpython/testtypes.py
@@ -1,5 +1,5 @@
 # Auto generated from testtypes.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-07-29 02:19
+# Generation date: 2021-08-14 01:44
 # Schema: types
 #
 # id: http://example.org/tests/types
