841a3bd3329ae2e6aad6c84357c8212001c83bc0
Sujay Patil <sujaysanjeev.patil@gmail.com>
2021-08-25 00:03:42 -0700

    generate empty excel sheet with class names

--- linkml/generators/excelgen.py
+++ linkml/generators/excelgen.py
@@ -1,3 +1,4 @@
+import os
 from typing import Union, TextIO, Optional
 
 import click
@@ -5,15 +6,40 @@ from openpyxl import Workbook
 
 from linkml.utils.generator import Generator, shared_arguments
 from linkml_runtime.linkml_model.meta import SchemaDefinition, ClassDefinition
-from linkml_runtime.utils.formatutils import camelcase, be
+from linkml_runtime.utils.formatutils import camelcase
 
 
 class ExcelGenerator(Generator):
+    generator_name = os.path.basename(__file__)
+    generator_version = "0.0.1"
     valid_formats = ["xlsx"]
 
-    def __init__(self, schema: Union[str, TextIO, SchemaDefinition], filename: Optional[str] = None, **kwargs) -> None:
-        self.wb_name = filename
+    def _workbook_path(self, wb_name: str = None):
+        if not wb_name:
+            return os.path.join(
+                os.path.basename(self.generator_name),
+                "_",
+                self.generator_version,
+                ".xlsx",
+            )
+
+        return os.path.join(
+            os.path.basename(self.generator_name),
+            "_",
+            self.generator_version,
+            "_",
+            ".xlsx",
+        )
+
+    def __init__(
+        self,
+        schema: Union[str, TextIO, SchemaDefinition],
+        filename: Optional[str] = None,
+        **kwargs
+    ) -> None:
+        self.wb_name = self._workbook_path(wb_name=filename)
         self.workbook = Workbook()
+        self.workbook.remove(self.workbook["Sheet"])
         super().__init__(schema, **kwargs)
 
     def create_spreadsheet(self, ws_name: str = None):
