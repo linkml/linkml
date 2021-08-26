a3435bec2fc905c710e355ca4023daf661a9a8d8
GitHub Action <action@github.com>
2021-08-26 20:55:35 +0000

    Automated adding outputs from tests

--- tests/test_issues/output/issue_44.py
+++ tests/test_issues/output/issue_44.py
@@ -1,5 +1,5 @@
 # Auto generated from issue_44.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-08-25 05:57
+# Generation date: 2021-08-26 20:54
 # Schema: test44
 #
 # id: https://example.com/test44
--- tests/test_scripts/output/genpython/ordering.py
+++ tests/test_scripts/output/genpython/ordering.py
@@ -1,5 +1,5 @@
 # Auto generated from ordering.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-08-25 05:58
+# Generation date: 2021-08-26 20:55
 # Schema: test_inherited_id
 #
 # id: https://example.org/inheritedid
