5f2cdc8ac91de717d9dc7d3d254ed04e0b7c5cda
GitHub Action <action@github.com>
2021-08-27 03:29:35 +0000

    Automated adding outputs from tests

--- tests/test_generators/output/ks/core.py
+++ tests/test_generators/output/ks/core.py
@@ -1,5 +1,5 @@
 # Auto generated from core.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-08-26 20:54
+# Generation date: 2021-08-27 03:27
 # Schema: core
 #
 # id: https://w3id.org/linkml/tests/core
--- tests/test_scripts/output/genpython/timepoint.py
+++ tests/test_scripts/output/genpython/timepoint.py
@@ -1,5 +1,5 @@
 # Auto generated from timepoint.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-08-26 20:55
+# Generation date: 2021-08-27 03:28
 # Schema: timepoint
 #
 # id: http://example.org/tests/timepoint
