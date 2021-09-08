b58461c3b635a42c62f76021a072f723adac7984
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2021-09-08 13:44:49 -0700

    add name to setup.py to enable dependents graph in github

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ if sys.version_info < (3, 7, 6):
     warn(f"Some URL processing will fail with python 3.7.5 or earlier.  Current version: {sys.version_info}")
 
 setup(
+    name='linkml',
     setup_requires=['pbr'],
     pbr=True,
 )
