46084a94e2ed0ad05724d40e9f6d59605c394fd9
GitHub Action <action@github.com>
2021-09-14 21:52:19 +0000

    Automated adding outputs from tests

--- tests/test_issues/output/issue_44.py
+++ tests/test_issues/output/issue_44.py
@@ -1,5 +1,5 @@
 # Auto generated from issue_44.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-09-14 04:57
+# Generation date: 2021-09-14 21:50
 # Schema: test44
 #
 # id: https://example.com/test44
