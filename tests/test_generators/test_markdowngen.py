841f3590718869998c8885bcbd30e1bdb63daf42
cmungall <cjm@berkeleybop.org>
2021-09-22 17:15:26 -0700

    Additional markdown tests for customized output

--- tests/test_generators/test_markdowngen.py
+++ tests/test_generators/test_markdowngen.py
@@ -46,6 +46,28 @@ class MarkdownGeneratorTestCase(unittest.TestCase):
         assert_mdfile_contains('has_medical_history.md', 'MedicalEvent', after='Domain and Range')
         assert_mdfile_contains('has_medical_history.md', 'subset B', after='Other properties')
 
+    def test_slot_name_mapping(self):
+        """ Tests rewiring names of main metamodel types  """
+        gen = MarkdownGenerator(SCHEMA, mergeimports=True, no_types_dir=True)
+        gen.metamodel_name_map = {"class": "record",
+                                  "slot": "field",
+                                  "mixin": "trait"}
+        self.assertEqual(gen.get_metamodel_slot_name('class'), 'record')
+        self.assertEqual(gen.get_metamodel_slot_name('classes'), 'records')
+        self.assertEqual(gen.get_metamodel_slot_name('Classes'), 'Records')
+        self.assertEqual(gen.get_metamodel_slot_name('Mixins'), 'Traits')
+
+        gen.metamodel_name_map = {"class": "Record",
+                                  "slot": "Field",
+                                   "mixin": "Trait"}
+        self.assertEqual(gen.get_metamodel_slot_name('class'), 'Record')
+        self.assertEqual(gen.get_metamodel_slot_name('classes'), 'Records')
+        self.assertEqual(gen.get_metamodel_slot_name('Classes'), 'Records')
+        self.assertEqual(gen.get_metamodel_slot_name('Mixins'), 'Traits')
+        md = gen.serialize(directory=MD_DIR)
+        assert_mdfile_contains('index.md', 'Address', after='Records')
+        assert_mdfile_contains('index.md', 'HasAliases', after='Traits')
+
 
 if __name__ == '__main__':
     unittest.main()
