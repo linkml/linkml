cd3698cdd3fc498b1ccf1b20d077f699f019611b
GitHub Action <action@github.com>
2021-09-23 18:34:19 +0000

    Automated adding outputs from tests

--- tests/test_scripts/output/genpython/timepoint.py
+++ tests/test_scripts/output/genpython/timepoint.py
@@ -1,5 +1,5 @@
 # Auto generated from timepoint.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-09-23 02:33
+# Generation date: 2021-09-23 18:33
 # Schema: timepoint
 #
 # id: http://example.org/tests/timepoint
