3bc7d407dd833aaefabb34e1d32ceb896a145b88
GitHub Action <action@github.com>
2021-09-30 03:30:46 +0000

    Automated adding outputs from tests

--- tests/test_scripts/output/genpython/inheritedid.py
+++ tests/test_scripts/output/genpython/inheritedid.py
@@ -1,5 +1,5 @@
 # Auto generated from inheritedid.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-09-30 01:43
+# Generation date: 2021-09-30 03:29
 # Schema: test_inherited_id
 #
 # id: https://example.org/inheritedid
