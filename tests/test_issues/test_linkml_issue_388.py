d1e2aa7e24a691c8d7a6f8a6f8bbd43644bb5253
cmungall <cjm@berkeleybop.org>
2021-10-01 18:40:12 -0700

    #388

--- tests/test_issues/test_linkml_issue_388.py
+++ tests/test_issues/test_linkml_issue_388.py
@@ -42,10 +42,19 @@ class IssueOWLNamespaceTestCase(TestEnvironmentTestCase):
                                                                importmap=env.import_map).serialize(context=[METAMODEL_CONTEXT_URI],),
                                       value_is_returned=True)
         self.env.generate_single_file(f'{name}.owl',
-                                      lambda: OwlSchemaGenerator(env.input_path(f'{name}.yaml')
+                                      lambda: OwlSchemaGenerator(env.input_path(f'{name}.yaml'),
+                                                                 metaclasses=False,
                                                                  ).serialize(context=[METAMODEL_CONTEXT_URI]),
                                       value_is_returned=True)
-        print(RDFGenerator(infile, mergeimports=False).serialize(context=[METAMODEL_CONTEXT_URI]))
+        g = Graph()
+        g.parse(env.expected_path(f'{name}.owl'), format='turtle')
+        C1 = URIRef('https://example.org/this/C1')
+        this_a = URIRef('https://example.org/this/a')
+        other_a = URIRef('https://example.org/other/a')
+        # slot_uri refers to two attributes, ambiguous, so minimal metadata;
+        assert len(list(g.triples((this_a, None, None)))) == 1
+        self.assertIn((other_a, RDFS.range, C1), g)
+
 
 
 if __name__ == '__main__':
