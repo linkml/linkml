adc73acc9b763280f732b043e51ba3deb0fdb9cf
Sujay Patil <sujaysanjeev.patil@gmail.com>
2021-10-19 22:40:05 -0700

    commit that lays out barebones for excel dropdown support

--- linkml/generators/excelgen.py
+++ linkml/generators/excelgen.py
@@ -2,10 +2,19 @@ import os
 from typing import Union, TextIO, Optional, List
 
 import click
-import openpyxl
+
+from openpyxl import Workbook, load_workbook
+from openpyxl.worksheet.datavalidation import DataValidation
 
 from linkml.utils.generator import Generator, shared_arguments
-from linkml_runtime.linkml_model.meta import SchemaDefinition, ClassDefinition
+from linkml_runtime.linkml_model.meta import (
+    SchemaDefinition,
+    ClassDefinition,
+    EnumDefinition,
+    PermissibleValue,
+    PermissibleValueText,
+    SlotDefinition,
+)
 from linkml_runtime.utils.formatutils import camelcase
 
 
@@ -56,8 +65,11 @@ class ExcelGenerator(Generator):
         **kwargs,
     ) -> None:
         self.wb_name = self._workbook_path(yaml_filename=schema, wb_name=output)
-        self.workbook = openpyxl.Workbook()
+        self.workbook = Workbook()
         self.workbook.remove(self.workbook["Sheet"])
+
+        # dictionary with slot types and possibles values for those types
+        self.enum_dict = {}
         super().__init__(schema, **kwargs)
 
     def _create_spreadsheet(self, ws_name: str, columns: List[str]) -> None:
@@ -74,10 +86,54 @@ class ExcelGenerator(Generator):
         self.workbook.save(self.wb_name)
 
     def visit_class(self, cls: ClassDefinition) -> bool:
-        """Overridden from generator framework."""
+        """Overridden method to intercept classes from generator framework."""
         self._create_spreadsheet(ws_name=camelcase(cls.name), columns=cls.slots)
 
         return True
+
+    def visit_enum(self, enum: EnumDefinition) -> bool:
+        """Overridden method to intercept enums from generator framework."""
+
+        def extract_permissible_text(pv):
+            if type(pv) is str:
+                return pv
+            if type(pv) is PermissibleValue:
+                return pv.text.code
+            if type(pv) is PermissibleValueText:
+                return pv
+            raise ValueError(f"Invalid permissible value in enum {enum}: {pv}")
+
+        permissible_values_texts = list(
+            map(extract_permissible_text, enum.permissible_values or [])
+        )
+
+        self.enum_dict[enum.name] = permissible_values_texts
+
+    def visit_class_slot(
+        self, cls: ClassDefinition, aliased_slot_name: str, slot: SlotDefinition
+    ) -> None:
+        """Overridden method to intercept classes and associated slots from generator 
+        framework."""
+        wb = load_workbook(self.wb_name)
+
+        if cls.name in self.workbook.sheetnames:
+            if slot.range in self.enum_dict:
+
+                # TODO: the below list needs to be dynamically populated with items 
+                # in dropdown
+                # basically the values associated with an enum type
+                valid = '"The,earth,revolves,around,sun"'
+
+                ws = self.workbook[cls.name]
+
+                dv = DataValidation(type="list", formula1=valid, allow_blank=True)
+                ws.add_data_validation(dv)
+
+                # TODO: select the columns from the sheet to which the dropdown is 
+                # to be applied
+                dv.add("B1")
+
+                wb.save(self.wb_name)
 
 
 @shared_arguments(ExcelGenerator)
