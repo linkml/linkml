d3072cef80683140b7169f0d32f16aaf23dca00b
GitHub Action <action@github.com>
2021-10-26 17:07:13 +0000

    Automated adding outputs from tests

--- tests/test_scripts/output/genpython/inheritedid_ncvs.py
+++ tests/test_scripts/output/genpython/inheritedid_ncvs.py
@@ -1,5 +1,5 @@
 # Auto generated from inheritedid.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-10-26 03:39
+# Generation date: 2021-10-26 17:06
 # Schema: test_inherited_id
 #
 # id: https://example.org/inheritedid
