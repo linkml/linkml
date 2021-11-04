56884afa8ff869b98d1f0e0be84579291cb8f635
Sujay Patil <sujaysanjeev.patil@gmail.com>
2021-11-04 14:39:51 -0700

    unit test for running prefixmapgen.py with format --tsv option

--- tests/test_prefixes/test_prefixes.py
+++ tests/test_prefixes/test_prefixes.py
@@ -3,11 +3,14 @@ import unittest
 import logging
 import json
 from rdflib import Graph, URIRef
+import tempfile
+import csv
 
 from linkml.generators.owlgen import OwlSchemaGenerator
 from linkml.generators.rdfgen import RDFGenerator
 from linkml.generators.prefixmapgen import PrefixGenerator
 from linkml.generators.jsonldcontextgen import ContextGenerator
+from tests.test_generators import output
 from tests.test_prefixes.environment import env
 
 SCHEMA = env.input_path('prefixtest.yaml')
@@ -15,6 +18,7 @@ OWL_OUTPUT = env.expected_path('prefixtest.owl.ttl')
 RDF_OUTPUT = env.expected_path('prefixtest.rdf.nt')
 PM_OUTPUT = env.expected_path('prefixtest.prefixmap.json')
 CONTEXT_OUTPUT = env.expected_path('prefixtest.context.jsonld')
+TSV_OUTPUT = env.expected_path('prefix_map_personinfo.tsv')
 
 
 class PrefixTestCase(unittest.TestCase):
@@ -80,6 +84,32 @@ class PrefixTestCase(unittest.TestCase):
                 logging.error(f'Missing key: {k}')
                 fails += 1
         assert fails == 0
+
+        # unit test when format option tsv is supplied
+        new_file, filename = tempfile.mkstemp()
+
+        tsv_filepath = filename + ".tsv"
+
+        PrefixGenerator(SCHEMA, format="tsv", output=tsv_filepath, mergeimports=True).serialize()
+
+        actual_tsv_dict = {}
+        with open(tsv_filepath) as fd:
+            rd = csv.reader(fd, delimiter="\t", quotechar='"')
+            for row in rd:
+                actual_tsv_dict[row[0]] = row[1]
+
+        actual_tsv_dict = dict(sorted(actual_tsv_dict.items()))
+
+        expected_tsv_dict = {}
+        with open(TSV_OUTPUT) as fd:
+            rd = csv.reader(fd, delimiter="\t", quotechar='"')
+            for row in rd:
+                expected_tsv_dict[row[0]] = row[1]
+
+        expected_tsv_dict = dict(sorted(expected_tsv_dict.items()))
+
+        self.assertDictEqual(actual_tsv_dict, expected_tsv_dict)
+
 
     def test_jsonldcontext(self):
         out = ContextGenerator(SCHEMA, mergeimports=True).serialize()
