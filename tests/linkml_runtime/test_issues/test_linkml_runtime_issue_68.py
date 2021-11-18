0be5a5b54ce01d73406bdaa3c5b6cf392311df35
Bill Duncan <wdduncan@gmail.com>
2021-11-18 16:03:41 -0500

    add unit test for #68

---
+++ tests/linkml_runtime/test_issues/test_linkml_runtime_issue_68.py
@@ -0,0 +1,53 @@
+import unittest
+from unittest import TestCase
+from linkml_runtime.utils.schemaview import SchemaView
+
+schema_str = """
+id: https://example.com/test-induced
+name: test-induced
+title: test induced
+
+description: >-
+    test schema view's finding of induced slot information.
+
+classes:
+    class1:
+        slots:
+            - slot1
+        slot_usage:
+            slot2:
+                 description: induced slot2
+                 required: true
+slots:
+    slot1:
+        description: non-induced slot1
+    slot2:
+        description: non-induced slot2
+        required: false
+    
+"""
+
+
+class Issue68TestCase(TestCase):
+    def test_issue_68(self):
+        view = SchemaView(schema_str)
+
+        # test descripton for slot1
+        s1 = view.get_slot("slot1")
+        assert s1.description == "non-induced slot1"
+
+        s2 = view.get_slot("slot2")
+        assert s2.required == False
+
+        s2_induced = view.induced_slot("slot2", "class1")
+        assert s2_induced.required == True
+
+        # test description for slot2
+        # this behavior is expected see: https://github.com/linkml/linkml-runtime/issues/68
+        s2_induced = view.induced_slot("slot2", "class1")
+        print("s2_induced.description:", s2_induced.description)
+        assert s2_induced.description == "induced slot2"
+
+
+if __name__ == "__main__":
+    unittest.main()
