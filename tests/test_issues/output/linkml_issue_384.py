6a1138921b59b64ccd6cb80162ab7e074449e274
GitHub Action <action@github.com>
2021-12-11 03:16:34 +0000

    Automated adding outputs from tests

--- tests/test_issues/output/linkml_issue_384.py
+++ tests/test_issues/output/linkml_issue_384.py
@@ -1,5 +1,5 @@
 # Auto generated from linkml_issue_384.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-12-07T16:34:03
+# Generation date: 2021-12-11T03:14:54
 # Schema: personinfo
 #
 # id: https://w3id.org/linkml/examples/personinfo
--- tests/test_scripts/output/genpython/ordering.py
+++ tests/test_scripts/output/genpython/ordering.py
@@ -1,5 +1,5 @@
 # Auto generated from ordering.yaml by pythongen.py version: 0.9.0
-# Generation date: 2021-12-07T16:34:50
+# Generation date: 2021-12-11T03:15:41
 # Schema: test_inherited_id
 #
 # id: https://example.org/inheritedid
