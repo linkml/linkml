be52c0905428a1dba1326d5a19468e0263cb8ed3
cmungall <cjm@berkeleybop.org>
2022-01-04 15:20:33 -0800

    Extending owlgenerator to export Enums Fixes #551

--- tests/test_generators/test_owlgen.py
+++ tests/test_generators/test_owlgen.py
@@ -1,6 +1,7 @@
 import sys
 import unittest
-from rdflib import Graph
+from rdflib import Graph, Namespace
+from rdflib.namespace import RDF, OWL
 
 from linkml.generators.owlgen import OwlSchemaGenerator
 from tests.test_generators.environment import env
@@ -10,15 +11,30 @@ DATA = env.input_path('kitchen_sink_inst_01.yaml')
 SHEXLOG = env.expected_path('owl_log.txt')
 OWL_OUTPUT = env.expected_path('kitchen_sink.owl.ttl')
 
+SYMP = Namespace('http://purl.obolibrary.org/obo/SYMP_')
+KS = Namespace('https://w3id.org/linkml/tests/kitchen_sink/')
+LINKML = Namespace('https://w3id.org/linkml/')
+BIZ = Namespace('https://example.org/bizcodes/')
 
 class OwlGeneratorTestCase(unittest.TestCase):
     def test_owlgen(self):
         """ owl  """
-        owl = OwlSchemaGenerator(SCHEMA, mergeimports=False, ontology_uri_suffix='.owl.ttl').serialize()
+        owl = OwlSchemaGenerator(SCHEMA,
+                                 mergeimports=False,
+                                 metaclasses=False,
+                                 type_objects=False,
+                                 ontology_uri_suffix='.owl.ttl').serialize(mergeimports=False)
         with open(OWL_OUTPUT, 'w') as stream:
             stream.write(owl)
         g = Graph()
         g.parse(OWL_OUTPUT, format="turtle")
+        owl_classes = list(g.subjects(RDF.type, OWL.Class))
+        for c in owl_classes:
+            print(f'Class={c}')
+        assert KS.MedicalEvent in owl_classes
+        # test that enums are treated as classes
+        assert KS.EmploymentEventType in owl_classes
+        assert BIZ['001'] in owl_classes
 
 
 
