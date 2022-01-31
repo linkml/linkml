cabcc6f8c1a36f8bb7923b0495f59b6fe2c031af
hsolbrig <solbrig@earthlink.net>
2022-01-31 17:15:30 -0600

    Fixed dataclass extensions to work with 3.7, 3.8, 3.9 and 3.10
    Note: If dataclass continues to change, we need to seriously consider changing our generators to
    produce explicit initialization code rather than trying to keep up with this.  The other possiblity
    would be to see whether we couldn't convince the dataclass authors to incorporate our 2 lines worth
    of change.

--- packages/linkml_runtime/utils/dataclass_extensions_376.py
+++ packages/linkml_runtime/utils/dataclass_extensions_376.py
@@ -7,7 +7,7 @@ elif sys.version_info >= (3, 10, 0):
         _create_fn
 
     def dataclasses_init_fn_with_kwargs(fields, std_fields, kw_only_fields, frozen, has_post_init,
-                                        self_name, globals, slots):
+                 self_name, globals, slots):
         # fields contains both real fields and InitVar pseudo-fields.
 
         # Make sure we don't have fields without defaults following fields
@@ -63,7 +63,7 @@ elif sys.version_info >= (3, 10, 0):
                           locals=locals,
                           globals=globals,
                           return_type=None)
-elif sys.version_info >= (3, 7, 6):
+elif sys.version_info >= (3, 9, 0):
     from dataclasses import MISSING, _HAS_DEFAULT_FACTORY, _POST_INIT_NAME, _FIELD_INITVAR, _init_param, _field_init, _create_fn
 
 
@@ -115,10 +115,9 @@ elif sys.version_info >= (3, 7, 6):
                           locals=locals,
                           globals=globals,
                           return_type=None)
-else:
+elif sys.version_info >= (3, 8, 0):
     from dataclasses import MISSING, _HAS_DEFAULT_FACTORY, _POST_INIT_NAME, _FIELD_INITVAR, _init_param, _field_init, \
         _create_fn
-
 
     def dataclasses_init_fn_with_kwargs(fields, frozen, has_post_init, self_name):
         # fields contains both real fields and InitVar pseudo-fields.
@@ -155,11 +154,63 @@ else:
                                   if f._field_type is _FIELD_INITVAR)
             body_lines.append(f'{self_name}.{_POST_INIT_NAME}({params_str}{", " if params_str else ""} **kwargs)')
 
-        # If no body lines, use 'pass'.
+    # If no body lines, use 'pass'.
         if not body_lines:
             body_lines = ['pass']
 
         locals = {f'_type_{f.name}': f.type for f in fields}
+        return _create_fn('__init__',
+                          [self_name] + [_init_param(f) for f in fields if f.init] + ["**kwargs"],
+                          body_lines,
+                          locals=locals,
+                          globals=globals,
+                          return_type=None)
+else:
+    from dataclasses import MISSING, _HAS_DEFAULT_FACTORY, _POST_INIT_NAME, _FIELD_INITVAR, _init_param, _field_init, \
+        _create_fn
+
+    def dataclasses_init_fn_with_kwargs(fields, frozen, has_post_init, self_name, globals):
+        # fields contains both real fields and InitVar pseudo-fields.
+
+        # Make sure we don't have fields without defaults following fields
+        # with defaults.  This actually would be caught when exec-ing the
+        # function source code, but catching it here gives a better error
+        # message, and future-proofs us in case we build up the function
+        # using ast.
+        seen_default = False
+        for f in fields:
+            # Only consider fields in the __init__ call.
+            if f.init:
+                if not (f.default is MISSING and f.default_factory is MISSING):
+                    seen_default = True
+                elif seen_default:
+                    raise TypeError(f'non-default argument {f.name!r} '
+                                    'follows default argument')
+
+        locals = {f'_type_{f.name}': f.type for f in fields}
+        locals.update({
+            'MISSING': MISSING,
+            '_HAS_DEFAULT_FACTORY': _HAS_DEFAULT_FACTORY,
+        })
+
+        body_lines = []
+        for f in fields:
+            line = _field_init(f, frozen, globals, self_name)
+            # line is None means that this field doesn't require
+            # initialization (it's a pseudo-field).  Just skip it.
+            if line:
+                body_lines.append(line)
+
+        # Does this class have a post-init function?
+        if has_post_init:
+            params_str = ','.join(f.name for f in fields
+                                  if f._field_type is _FIELD_INITVAR)
+            body_lines.append(f'{self_name}.{_POST_INIT_NAME}({params_str}{", " if params_str else ""} **kwargs)')
+
+        # If no body lines, use 'pass'.
+        if not body_lines:
+            body_lines = ['pass']
+
         return _create_fn('__init__',
                           [self_name] + [_init_param(f) for f in fields if f.init] + ["**kwargs"],
                           body_lines,
