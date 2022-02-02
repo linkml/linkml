95072a7ced60ddf69f6796c1de06c9229d16e7b7
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-02-01 16:37:35 -0800

    test case for issue 590

---
+++ tests/linkml_runtime/test_issues/linkml_test_issue_590.py
@@ -0,0 +1,22 @@
+import unittest
+
+from linkml_runtime.utils.schemaview import SchemaView
+
+from tests.test_issues.environment import env
+
+SCHEMA = env.input_path('linkml_issue_590.yaml')
+
+
+class Issue590TestCase(unittest.TestCase):
+    env = env
+
+    def test_issue_590(self):
+        """get_slot() returns attribute not top level slot"""
+        sv = SchemaView(SCHEMA)
+
+        # check that multivalued is set to False as in schema
+        self.assertEqual(sv.get_slot('a').multivalued, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
