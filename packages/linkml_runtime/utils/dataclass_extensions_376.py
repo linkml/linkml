ee1e26c5e6b332ec3c35a57db9e3cbbb339f5f7b
hsolbrig <solbrig@earthlink.net>
2022-02-07 15:15:54 -0800

    Deal with 3.9
    See previous note about multitasking

--- packages/linkml_runtime/utils/dataclass_extensions_376.py
+++ packages/linkml_runtime/utils/dataclass_extensions_376.py
@@ -232,7 +232,63 @@ elif sys.version_info.minor == 8 and sys.version_info.micro > 0:
         if has_post_init:
             params_str = ','.join(f.name for f in fields
                                   if f._field_type is _FIELD_INITVAR)
-            body_lines.append(f'{self_name}.{_POST_INIT_NAME}({params_str})')
+            # This line is changed
+            body_lines.append(f'{self_name}.{_POST_INIT_NAME}({params_str}{", " if params_str else ""} **kwargs)')
+
+
+        # If no body lines, use 'pass'.
+        if not body_lines:
+            body_lines = ['pass']
+
+        return _create_fn('__init__',
+                          # This line is changed
+                          [self_name] + [_init_param(f) for f in fields if f.init] + ["**kwargs"],
+                          body_lines,
+                          locals=locals,
+                          globals=globals,
+                          return_type=None)
+elif sys.version_info.minor == 9:
+    # Taken from 3.9.10
+    # Dataclass takes 5 arguments
+    def dataclasses_init_fn_with_kwargs(fields, frozen, has_post_init, self_name, globals):
+        # fields contains both real fields and InitVar pseudo-fields.
+
+        # Make sure we don't have fields without defaults following fields
+        # with defaults.  This actually would be caught when exec-ing the
+        # function source code, but catching it here gives a better error
+        # message, and future-proofs us in case we build up the function
+        # using ast.
+        seen_default = False
+        for f in fields:
+            # Only consider fields in the __init__ call.
+            if f.init:
+                if not (f.default is MISSING and f.default_factory is MISSING):
+                    seen_default = True
+                elif seen_default:
+                    raise TypeError(f'non-default argument {f.name!r} '
+                                    'follows default argument')
+
+        locals = {f'_type_{f.name}': f.type for f in fields}
+        locals.update({
+            'MISSING': MISSING,
+            '_HAS_DEFAULT_FACTORY': _HAS_DEFAULT_FACTORY,
+        })
+
+        body_lines = []
+        for f in fields:
+            line = _field_init(f, frozen, locals, self_name)
+            # line is None means that this field doesn't require
+            # initialization (it's a pseudo-field).  Just skip it.
+            if line:
+                body_lines.append(line)
+
+        # Does this class have a post-init function?
+        if has_post_init:
+            params_str = ','.join(f.name for f in fields
+                                  if f._field_type is _FIELD_INITVAR)
+            # This line is changed
+            body_lines.append(f'{self_name}.{_POST_INIT_NAME}({params_str}{", " if params_str else ""} **kwargs)')
+
 
         # If no body lines, use 'pass'.
         if not body_lines:
