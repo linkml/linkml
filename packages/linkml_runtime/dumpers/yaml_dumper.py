93a57fe2d31a3845a5b5351e0ebd64f10c176fb3
David Linke <dr.david.linke@gmail.com>
2022-03-05 17:18:38 +0100

    Write strings to utf-8 encoded yaml files

--- packages/linkml_runtime/dumpers/yaml_dumper.py
+++ packages/linkml_runtime/dumpers/yaml_dumper.py
@@ -13,6 +13,4 @@ class YAMLDumper(Dumper):
         # Internal note: remove_empty_items will also convert Decimals to int/float;
         # this is necessary until https://github.com/yaml/pyyaml/pull/372 is merged
         return yaml.dump(remove_empty_items(element, hide_protected_keys=True),
-                         Dumper=yaml.SafeDumper, sort_keys=False, 
-                         allow_unicode=True,
-                         **kwargs)
+                                            Dumper=yaml.SafeDumper, sort_keys=False, **kwargs)
