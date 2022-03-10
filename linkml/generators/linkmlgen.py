5195db19ac88b89779cf7d4be4049c8e48f83470
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-03-10 12:32:23 -0800

    revamped JSON, YAML generator using gen-linkml

--- linkml/generators/linkmlgen.py
+++ linkml/generators/linkmlgen.py
@@ -1,12 +1,11 @@
 import logging
 import os
-import json
-from typing import Dict, List, TextIO, Union
+from typing import TextIO, Union
 
 import click
 
-from linkml_runtime.dumpers import json_dumper
-from linkml_runtime.linkml_model.meta import SchemaDefinition, ClassDefinition
+from linkml_runtime.dumpers import json_dumper, yaml_dumper
+from linkml_runtime.linkml_model.meta import SchemaDefinition
 from linkml_runtime.utils.schemaview import SchemaView
 from linkml.utils.generator import Generator, shared_arguments
 
@@ -22,92 +21,64 @@ class JsonGenerator(Generator):
 
     generatorname = os.path.basename(__file__)
     generatorversion = "1.0.0"
-    valid_formats = ["json"]
+    valid_formats = ["json", "yaml"]
 
     def __init__(
         self,
         schema: Union[str, TextIO, SchemaDefinition],
         materialize_attributes: bool,
+        mergeimports: bool,
+        format: str,
         **kwargs,
     ):
         self.schemaview = SchemaView(schema)
         self.materialize = materialize_attributes
+        self.format = format
 
-    def materialized_classes(self) -> List[ClassDefinition]:
+        if mergeimports:
+            self.schemaview.merge_imports()
+
+    def materialize_classes(self) -> None:
         """Return list of ClassDefinition objects with each of
         the classes having their slots materialized as attribues
-
-        :return: List of ClassDefinition objects
-        :rtype: List[ClassDefinition]
         """
         all_classes = self.schemaview.all_classes()
-
-        # class definition objects with slots materialized as attributes
-        c_def_list = []
 
         for c_name, c_def in all_classes.items():
             attrs = self.schemaview.class_induced_slots(c_name)
             for attr in attrs:
                 c_def.attributes[attr.name] = attr
-            c_def.slots = []
-            c_def_list.append(c_def)
 
-        return c_def_list
+        return
 
-    def json(self) -> List[Dict[str, Union[str, dict]]]:
-        """Create list of JSON strings in dictionary format
-
-        :return: List of JSON dicts
-        :rtype: List[Dict[str, Union[str, dict]]]
-        """        
-        json_str_list = []
-
+    def serialize(self, **args) -> str:
         if self.materialize:
-            for c_def in self.materialized_classes():
-                json_str_list.append(json_dumper.dumps(c_def))
+            self.materialize_classes()
+
+        if self.format == "json":
+            return json_dumper.dumps(self.schemaview.schema)
+        elif self.format == "yaml":
+            return yaml_dumper.dumps(self.schemaview.schema)
         else:
-            for c_name, c_def in self.schemaview.all_classes().items():
-                json_str_list.append(json_dumper.dumps(c_def))
-
-        json_dict_list = []
-
-        for json_str in json_str_list:
-            json_dict_list.append(json.loads(json_str))
-
-        return json_dict_list
-
-    def serialize(
-        self, output=None, **args
-    ) -> Union[str, Dict[str, Union[str, List[str], Dict]]]:
-        json_dict_list = self.json()
-
-        if output:
-            with open(output, "w") as f:
-                json.dump(
-                    json_dict_list,
-                    f,
-                    sort_keys=False,
-                    ensure_ascii=False,
-                    indent=4,
-                    separators=(",", ": "),
-                )
-            logger.info(f"The JSON file has been written to {output}")
-        else:
-            return json_dict_list
+            raise ValueError(
+                f"{self.format} is an invalid format. Use one of the following "
+                f"formats: {self.valid_formats}"
+            )
 
 
 @shared_arguments(JsonGenerator)
 @click.option(
     "--materialize-attributes/--no-materialize-attributes",
     default=True,
-    required=True,
+    show_default=True,
     help="Materialize induced slots as attributes",
 )
-@click.option("--output", "-o", type=click.Path(), help="Path to output JSON file")
 @click.command()
-def cli(yamlfile, materialize_attributes, output, **args):
-    gen = JsonGenerator(yamlfile, materialize_attributes, **args)
-    print(gen.serialize(output))
+def cli(yamlfile, materialize_attributes, **kwargs):
+    gen = JsonGenerator(
+        yamlfile, materialize_attributes=materialize_attributes, **kwargs
+    )
+    print(gen.serialize())
 
 
 if __name__ == "__main__":
