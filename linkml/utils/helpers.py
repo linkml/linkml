4c3663a0f56b09bd2f47fc2a15877bb3cfa7a264
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-03-11 13:34:17 -0800

    encoding argument to write_to_file helper methods module

--- linkml/utils/helpers.py
+++ linkml/utils/helpers.py
@@ -2,7 +2,6 @@ def remove_duplicates(lst):
     """Remove duplicate tuples from a list of tuples."""
     return [t for t in (set(tuple(i) for i in lst))]
 
-def write_to_file(file_path, data, mode="w"):
-    """Write string data to a file."""
-    with open(file_path, mode) as f:
+def write_to_file(file_path, data, mode="w", encoding="utf-8"):
+    with open(file_path, mode, encoding=encoding) as f:
         f.write(data)
