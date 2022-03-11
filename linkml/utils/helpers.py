706ea1160c270e3e2aa44bfa5b5e05bfbd99c476
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-03-11 01:19:32 -0800

    add helpers module from utils

--- linkml/utils/helpers.py
+++ linkml/utils/helpers.py
@@ -1,3 +1,8 @@
 def remove_duplicates(lst):
     """Remove duplicate tuples from a list of tuples."""
     return [t for t in (set(tuple(i) for i in lst))]
+
+def write_to_file(file_path, data, mode="w"):
+    """Write string data to a file."""
+    with open(file_path, mode) as f:
+        f.write(data)
