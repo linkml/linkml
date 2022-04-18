567d7b7848e5f68b3f96fb1f040bb313a4b929b7
cmungall <cjm@berkeleybop.org>
2022-04-18 13:03:27 -0700

    fixing test for #692

--- tests/test_issues/test_linkml_issue_692.py
+++ tests/test_issues/test_linkml_issue_692.py
@@ -3,7 +3,7 @@ import unittest
 from rdflib import URIRef, Graph
 from rdflib.namespace import RDFS, DCTERMS
 
-from linkml.generators.owlgen import OwlSchemaGenerator
+from linkml.generators.owlgen import OwlSchemaGenerator, MetadataProfile
 
 from tests.utils.test_environment import TestEnvironmentTestCase
 from tests.test_issues.environment import env
@@ -39,27 +39,34 @@ slots:
 class IssueDescriptionExportCase(TestEnvironmentTestCase):
     env = env
 
-    # export the source schema containing both title and description
-    gen = OwlSchemaGenerator(schema_str, None, False, False, True, format="ttl")
-    output = gen.serialize()
+    def test_owlgen_rdfs_profile(self):
+        # export the source schema containing both title and description
+        gen = OwlSchemaGenerator(schema_str,
+                                 ontology_uri_suffix=None,
+                                 type_objects=False,
+                                 metaclasses=False,
+                                 add_ols_annotations=True,
+                                 metadata_profile=MetadataProfile.rdfs,
+                                 format="ttl")
+        output = gen.serialize()
 
-    # load back via rdflib
-    graph = Graph(base="http://example.org/description-export")
-    graph.parse(data=output, format="ttl")
+        # load back via rdflib
+        graph = Graph(base="http://example.org/description-export")
+        graph.parse(data=output, format="ttl")
 
-    # check if graph contains dcterms:title for class 'Person'
-    person_class = URIRef(f"{graph.base}/Person")
-    assert (person_class, DCTERMS.title, None) in graph
+        # check if graph contains dcterms:title for class 'Person'
+        person_class = URIRef(f"{graph.base}/Person")
+        assert (person_class, DCTERMS.title, None) in graph
 
-    # check if graph contains dcterms:title for property 'name'
-    name_prop = URIRef(f"{graph.base}/name")
-    assert (name_prop, DCTERMS.title, None) in graph
+        # check if graph contains dcterms:title for property 'name'
+        name_prop = URIRef(f"{graph.base}/name")
+        assert (name_prop, DCTERMS.title, None) in graph
 
-    # now check rdfs:comment for class 'Person'
-    assert (person_class, RDFS.comment, None) in graph
+        # now check rdfs:comment for class 'Person'
+        assert (person_class, RDFS.comment, None) in graph
 
-    # and check rdfs:comment for property 'name' too
-    assert (name_prop, RDFS.comment, None) in graph
+        # and check rdfs:comment for property 'name' too
+        assert (name_prop, RDFS.comment, None) in graph
 
 
 
