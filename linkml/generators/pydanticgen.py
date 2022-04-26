e4939e9841066867cb25e880fffb780aea163583
Kevin Schaper <kevinschaper@gmail.com>
2022-04-26 14:36:58 -0700

    Implement mixin inheritance in pydanticgen (#714)
    * Implement mixin inheritance, use sorted classes in reverse to get a working method resolution order
    
    * Adds option to not generate mixin inheritance, SQLAlchemy takes advantage of that option

--- linkml/generators/pydanticgen.py
+++ linkml/generators/pydanticgen.py
@@ -57,8 +57,7 @@ class {{ e.name }}(str, Enum):
 {%- for c in schema.classes.values() %}
 @dataclass(config=PydanticConfig)
 class {{ c.name }} 
-                   {%- if c.is_a %}({{c.is_a}}){% endif -%}
-                   {#- {%- for p in c.mixins %}, "{{p}}" {% endfor -%} -#} 
+                   {%- if class_isa_plus_mixins[c.name] %}({{class_isa_plus_mixins[c.name]|join(', ')}}){% endif -%}                   
                   :
     {% if c.description -%}
     \"\"\"
@@ -115,12 +114,18 @@ class PydanticGenerator(OOCodeGenerator):
                  template_file: str = None,
                  allow_extra = False,
                  format: str = valid_formats[0],
-                 genmeta: bool=False, gen_classvars: bool=True, gen_slots: bool=True, **kwargs) -> None:
+                 genmeta: bool=False,
+                 gen_classvars: bool=True,
+                 gen_slots: bool=True,
+                 gen_mixin_inheritance: bool = True,
+                 **kwargs) -> None:
+        self.sorted_class_names = None
         self.sourcefile = schema
         self.schemaview = SchemaView(schema)
         self.schema = self.schemaview.schema
         self.template_file = template_file
         self.allow_extra = allow_extra
+        self.gen_mixin_inheritance = gen_mixin_inheritance
 
     def map_type(self, t: TypeDefinition) -> str:
         return TYPEMAP.get(t.base, t.base)
@@ -129,10 +134,8 @@ class PydanticGenerator(OOCodeGenerator):
         # TODO: make an explicit class to represent how an enum is passed to the template
         enums = {}
         for enum_name, enum_orignal in all_enums.items():
-            enum = {}
-            enum['name'] = camelcase(enum_name)
+            enum = {'name': camelcase(enum_name), 'values': {}}
 
-            enum['values'] = {}
             for pv in enum_orignal.permissible_values.values():
                 label = self.generate_enum_label(pv.text)
                 enum['values'][label] = pv.text
@@ -196,6 +199,26 @@ class PydanticGenerator(OOCodeGenerator):
                     slot_values[camelcase(class_def.name)][slot.name] = "default_factory=list"
         return slot_values
 
+    def get_class_isa_plus_mixins(self) -> Dict[str, List[str]]:
+        """
+        Generate the inheritance list for each class from is_a plus mixins
+        :return:
+        """
+        sv = self.schemaview
+        parents = {}
+        for class_def in sv.all_classes().values():
+            class_parents = []
+            if class_def.is_a:
+                class_parents.append(camelcase(class_def.is_a))
+            if self.gen_mixin_inheritance and class_def.mixins:
+                class_parents.extend([camelcase(mixin) for mixin in class_def.mixins])
+            if len(class_parents) > 0:
+                # Use the sorted list of classes to order the parent classes, but reversed to match MRO needs
+                class_parents.sort(key=lambda x: self.sorted_class_names.index(x))
+                class_parents.reverse()
+                parents[camelcase(class_def.name)] = class_parents
+        return parents
+
     def serialize(self) -> str:
         sv = self.schemaview
 
@@ -213,6 +236,7 @@ class PydanticGenerator(OOCodeGenerator):
         enums = self.generate_enums(sv.all_enums())
 
         sorted_classes = self.sort_classes(list(sv.all_classes().values()))
+        self.sorted_class_names = [camelcase(c.name) for c in sorted_classes]
 
         # Don't want to generate classes when class_uri is linkml:Any, will
         # just swap in typing.Any instead down below
@@ -273,7 +297,10 @@ class PydanticGenerator(OOCodeGenerator):
                     pyrange = f'Optional[{pyrange}]'
                 ann = Annotation('python_range', pyrange)
                 s.annotations[ann.tag] = ann
-        code = template_obj.render(schema=pyschema, underscore=underscore, enums=enums, predefined_slot_values=self.get_predefined_slot_values(), allow_extra=self.allow_extra, metamodel_version=self.schema.metamodel_version, version=self.schema.version)
+        code = template_obj.render(schema=pyschema, underscore=underscore, enums=enums,
+                                   predefined_slot_values=self.get_predefined_slot_values(),
+                                   allow_extra=self.allow_extra, metamodel_version=self.schema.metamodel_version,
+                                   version=self.schema.version, class_isa_plus_mixins=self.get_class_isa_plus_mixins())
         return code
 
 
