6aadb5765419d041e35d0bb0cbbb640a544445e8
David Linke <dr.david.linke@gmail.com>
2022-05-13 22:16:46 +0200

    Make test_merge_contexts windows-compatible

--- tests/test_utils/test_context_utils.py
+++ tests/test_utils/test_context_utils.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import PurePath
 import unittest
 
 from jsonasobj2 import JsonObj, loads
@@ -8,9 +10,11 @@ from linkml_runtime.utils.context_utils import merge_contexts
 json_1 = '{ "ex": "http://example.org/test/", "ex2": "http://example.org/test2/" }'
 json_2 = '{ "foo": 17, "@context": { "ex": "http://example.org/test3/", "ex2": {"@id": "http://example.org/test4/" }}}'
 
+local_jsonld_uri = PurePath(os.path.abspath("local.jsonld")).as_uri()
+
 context_output = """{
    "@context": [
-      "file://local.jsonld",
+      "%s",
       "https://w3id.org/linkml/meta.context.jsonld",
       {
          "ex": "http://example.org/test/",
@@ -23,14 +27,14 @@ context_output = """{
          }
       }
    ]
-}"""
+}""" % local_jsonld_uri
 
 
 class ContextUtilsTestCase(unittest.TestCase):
     def test_merge_contexts(self):
         self.assertIsNone(merge_contexts())
-        self.assertEqual('file://local.jsonld', merge_contexts("local.jsonld")['@context'])
-        self.assertEqual('file://local.jsonld', merge_contexts(["local.jsonld"])['@context'])
+        self.assertEqual(local_jsonld_uri, merge_contexts("local.jsonld")['@context'])
+        self.assertEqual(local_jsonld_uri, merge_contexts(["local.jsonld"])['@context'])
         self.assertEqual(METAMODEL_CONTEXT_URI, merge_contexts(METAMODEL_CONTEXT_URI)['@context'])
         self.assertEqual(METAMODEL_CONTEXT_URI, merge_contexts([METAMODEL_CONTEXT_URI])['@context'])
         self.assertEqual(JsonObj(ex='http://example.org/test/', ex2='http://example.org/test2/'),
@@ -41,7 +45,7 @@ class ContextUtilsTestCase(unittest.TestCase):
                          merge_contexts(json_2)['@context'])
         self.assertEqual(JsonObj(ex='http://example.org/test3/', ex2=JsonObj(**{'@id': 'http://example.org/test4/'})),
                          merge_contexts([json_2])['@context'])
-        self.assertEqual([f'file://local.jsonld',
+        self.assertEqual([local_jsonld_uri,
                           'https://w3id.org/linkml/meta.context.jsonld',
                           JsonObj(ex='http://example.org/test/', ex2='http://example.org/test2/'),
                           JsonObj(ex='http://example.org/test3/', ex2=JsonObj(**{'@id': 'http://example.org/test4/'}))],
@@ -54,7 +58,7 @@ class ContextUtilsTestCase(unittest.TestCase):
             JsonObj(**{'@context': [JsonObj(ex='http://example.org/test/', ex2='http://example.org/test2/'),
                                     JsonObj(ex='http://example.org/test/', ex2='http://example.org/test2/')]}),
             merge_contexts([json_1, json_1]))
-        self.assertEqual('file://local.jsonld', merge_contexts("local.jsonld")['@context'])
+        self.assertEqual(local_jsonld_uri, merge_contexts("local.jsonld")['@context'])
 
     def test_merge_contexts_base(self):
         self.assertEqual(
