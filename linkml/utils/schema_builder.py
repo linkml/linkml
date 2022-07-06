6775314cd165fab140b7d4daade8aa85310ee2c4
Chris Mungall <cjm@berkeleybop.org>
2022-07-06 16:00:33 -0700

    Adding SchemaFixer, this is a general purpose utility for enhancing schemas. (#854)
    Includes:
    
    - add titles by de-camelify-snakifying names
    - add a default container class
    - add index slots
    - convert slots to attributes
    - remove redundant slot usage
    
    This library will grow to include common checks as well, and can be seen
    as an analog of flake8/black.
    
    TBD: move into its own repo?

--- linkml/utils/schema_builder.py
+++ linkml/utils/schema_builder.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass, field
 from typing import Union, List, Dict
 
-from linkml_runtime.linkml_model import SchemaDefinition, ClassDefinition, SlotDefinition, Prefix
-
-
-# TODO: move to linkml_runtime
+from linkml_runtime.linkml_model import SchemaDefinition, ClassDefinition, SlotDefinition, Prefix, PermissibleValue, \
+    EnumDefinition
+from linkml_runtime.utils.formatutils import camelcase, underscore
 
 
 @dataclass
@@ -12,11 +11,18 @@ class SchemaBuilder:
     """
     Builder class for SchemaDefinitions
     """
-    schema: SchemaDefinition = field(default_factory=lambda: SchemaDefinition(id='http://example.org/test/',
-                                                                              name='test-schema'))
+    name: str = None
+    schema: SchemaDefinition = None
+
+    def __post_init__(self):
+        name = self.name
+        if name is None:
+            name = 'test-schema'
+        self.schema = SchemaDefinition(id=f'http://example.org/{name}',
+                                       name=name)
 
     def add_class(self, cls: Union[ClassDefinition, str], slots: List[Union[str, SlotDefinition]] = None,
-                  slot_usage: Dict[str, SlotDefinition] = None, **kwargs) -> "SchemaBuilder":
+                  slot_usage: Dict[str, SlotDefinition] = None, use_attributes=False, **kwargs) -> "SchemaBuilder":
         """
         Adds a class to the schema
 
@@ -24,17 +30,24 @@ class SchemaBuilder:
         :param slots:
         :param slot_usage:
         :param kwargs:
-        :return:
+        :return: builder
         """
         if not isinstance(cls, ClassDefinition):
             cls = ClassDefinition(cls)
         self.schema.classes[cls.name] = cls
-        if slots is not None:
+        if use_attributes:
             for s in slots:
-                self.add_slot(s, cls.name)
-        if slot_usage:
-            for k, v in slot_usage.items():
-                cls.slot_usage[k] = v
+                if isinstance(s, SlotDefinition):
+                    cls.attributes[s.name] = s
+                else:
+                    raise ValueError(f'If use_attributes=True then slots must be SlotDefinitions')
+        else:
+            if slots is not None:
+                for s in slots:
+                    self.add_slot(s, cls.name)
+            if slot_usage:
+                for k, v in slot_usage.items():
+                    cls.slot_usage[k] = v
         for k, v in kwargs.items():
             setattr(cls, k, v)
         return self
@@ -45,7 +58,7 @@ class SchemaBuilder:
 
         :param slot:
         :param class_name: if specified, this will become a valid slot for the class
-        :return:
+        :return: builder
         """
         if isinstance(slot, str):
             slot = SlotDefinition(slot)
@@ -62,34 +75,59 @@ class SchemaBuilder:
 
         :param slot_name:
         :param kwargs:
-        :return:
+        :return: builder
         """
         slot = self.schema.slots[slot_name]
         for k, v in kwargs.items():
             setattr(slot, k, v)
         return self
 
-    def add_prefix(self, prefix: str, url: str):
+    def add_enum(self, enum_def: Union[EnumDefinition, str], permissible_values: List[Union[str, PermissibleValue]] = None
+                  ) -> "SchemaBuilder":
+        """
+        Adds an enum to the schema
+
+        :param enum_def:
+        :param permissible_values:
+        :return: builder
+        """
+        if not isinstance(enum_def, EnumDefinition):
+            enum_def = EnumDefinition(enum_def)
+        self.schema.enums[enum_def.name] = enum_def
+        if permissible_values is not None:
+            for pv in permissible_values:
+                if isinstance(pv, str):
+                    pv = PermissibleValue(text=pv)
+                    enum_def.permissible_values[pv.text] = pv
+        return self
+
+    def add_prefix(self, prefix: str, url: str) -> "SchemaBuilder":
         """
         Adds a prefix for use with CURIEs
 
         :param prefix:
         :param url:
-        :return:
+        :return: builder
         """
         obj = Prefix(prefix_prefix=prefix, prefix_reference=url)
         self.schema.prefixes[obj.prefix_prefix] = obj
+        return self
 
-    def add_defaults(self):
+    def add_defaults(self) -> "SchemaBuilder":
         """
         Sets defaults, including:
 
         - default_range
         - default imports to include linkml:types
         - default prefixes
+
+        :return: builder
         """
+        name = underscore(self.schema.name)
+        uri = self.schema.id
         self.schema.default_range = "string"
-        self.schema.default_prefix = "ex"
+        self.schema.default_prefix = name
         self.schema.imports.append("linkml:types")
         self.add_prefix("linkml", "https://w3id.org/linkml/")
-        self.add_prefix("ex", "https://example.org/test/")
+        self.add_prefix(name, f"{uri}/")
+        return self
