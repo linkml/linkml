6bcf904e09f275433cf2a698a38bf58187c9122e
cmungall <cjm@berkeleybop.org>
2023-01-20 16:40:48 -0800

    Adapt ERDiagram test to sync with test schema

--- tests/test_generators/test_erdiagramgen.py
+++ tests/test_generators/test_erdiagramgen.py
@@ -1,16 +1,6 @@
 import unittest
-from collections import namedtuple
-
-import yaml
-from linkml_runtime import SchemaView
-from linkml_runtime.dumpers import yaml_dumper
-from linkml_runtime.linkml_model import SlotDefinition
-from linkml_runtime.utils.compile_python import compile_python
-from pydantic import ValidationError
 
 from linkml.generators.erdiagramgen import ERDiagramGenerator
-from linkml.generators.pydanticgen import PydanticGenerator
-from linkml.utils.schema_builder import SchemaBuilder
 from tests.test_generators.environment import env
 
 SCHEMA = env.input_path("kitchen_sink.yaml")
@@ -26,6 +16,7 @@ Person {
     string name
     integer age_in_years
     string species_name
+    string species_formal_name
     integer stomach_count
     LifeStatusEnum is_living
     stringList aliases
