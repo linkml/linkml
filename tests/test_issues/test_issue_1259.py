9ffb0e20b976da1df5a01e0248805dab2422bd55
Mark A. Miller <MAM@lbl.gov>
2023-01-23 18:27:17 -0500

    slight reformat?

--- tests/test_issues/test_issue_1259.py
+++ tests/test_issues/test_issue_1259.py
@@ -1,7 +1,6 @@
 import unittest
 
 from jsonschema.exceptions import ValidationError
-from linkml_runtime.dumpers import yaml_dumper
 from linkml_runtime.loaders import yaml_loader
 
 from linkml.generators.pythongen import PythonGenerator
@@ -18,21 +17,6 @@ imports:
   - linkml:types
 
 classes:
-  Person:
-    rules:
-      - description: can't have any daughters if you don't have any children
-        preconditions:
-          slot_conditions:
-            daughters_count:
-              minimum_value: 2
-        postconditions:
-          slot_conditions:
-            children_count:
-              minimum_value: 2
-    slots:
-      - id
-      - children_count
-      - daughters_count
       
   Biosample:
     rules:
@@ -57,7 +41,7 @@ classes:
         postconditions:
           slot_conditions:
             plate_position:
-              pattern: ^(?!A1|A12|H1|H12)(([A-H][1-9])|([A-H]1[0-2]))$
+              pattern: ^(?!A1|A12|H1|H12)(([A-H][1-9])|([A-H]1[0-2]))$      
 
     slots:
       - id
@@ -66,33 +50,19 @@ classes:
 
 slots:
     id: {}
-    children_count:
-      range: integer
-    daughters_count:
-      range: integer
     container_type:
       range: ContainerTypeEnum
       required: true
     plate_position:
       range: string
+    plate_height_mm:
+      range: float
             
 enums:
   ContainerTypeEnum:
     permissible_values:
       TUBE: {}
       PLATE: {}
-"""
-
-PERSON_VALID = """
-id: "person:1"
-children_count: 4
-daughters_count: 3
-"""
-
-PERSON_INVALID = """
-id: "person:1"
-daughters_count: 4
-children_count: 1
 """
 
 TUBE_VALID = """
@@ -132,34 +102,23 @@ class MinRulesTestCase(unittest.TestCase):
         validator = JsonSchemaDataValidator(schema=SCHEMA)
         return dynamic_class, validator, obj
 
-    def run_valid_test(self, class_name, source):
+    def run_felxible_test(self, class_name, source, expected_pass=True):
         dynamic_class, validator, obj = self.get_dynclass_validator_object(dynamic_class_name=class_name, source=source)
-        validator.validate_object(obj)
 
-    def run_invalid_test(self, class_name, source):
-        dynamic_class, validator, obj = self.get_dynclass_validator_object(dynamic_class_name=class_name, source=source)
-        with self.assertRaises(ValidationError) as context:
+        if expected_pass:
             validator.validate_object(obj)
-
-    def test_instantiate_valid_person(self):
-        self.run_valid_test(class_name="Person", source=PERSON_VALID)
-
-    def test_invalid_person_exception_expected(self):
-        dynamic_class, validator, obj = self.get_dynclass_validator_object(dynamic_class_name="Person", source=
-        PERSON_INVALID)
-        print(yaml_dumper.dumps(obj))
-
-        # with self.assertRaises(Exception) as context:
-        #     validator.validate_object(person_obj)
+        else:
+            with self.assertRaises(ValidationError) as context:
+                validator.validate_object(obj)
 
     def test_tube_valid(self):
-        self.run_valid_test(class_name="Biosample", source=TUBE_VALID)
+        self.run_felxible_test(class_name="Biosample", source=TUBE_VALID, expected_pass=True)
 
     def test_tube_invalid(self):
-        self.run_invalid_test(class_name="Biosample", source=TUBE_INVALID)
+        self.run_felxible_test(class_name="Biosample", source=TUBE_INVALID, expected_pass=False)
 
     def test_plate_valid(self):
-        self.run_valid_test(class_name="Biosample", source=PLATE_VALID)
+        self.run_felxible_test(class_name="Biosample", source=PLATE_VALID, expected_pass=True)
 
     def test_plate_invalid(self):
-        self.run_invalid_test(class_name="Biosample", source=PLATE_INVALID)
+        self.run_felxible_test(class_name="Biosample", source=PLATE_INVALID, expected_pass=False)
