f4b8dab748c3c5306ce3e9c6807fe8760245835d
Kevin Schaper <kevinschaper@gmail.com>
2023-01-30 10:44:29 -0800

    Adds a test for multivalued any_of collections, without inlining, inlined and inlined as list

--- tests/test_generators/test_pydanticgen.py
+++ tests/test_generators/test_pydanticgen.py
@@ -101,6 +101,63 @@ enums:
         )
         assert enum["values"]["Ohio"] == "Ohio"
 
+    def test_pydantic_any_of(self):
+        # TODO: convert to SchemaBuilder and parameterize?
+        schema_str = """
+id: test_schema
+name: test_info
+description: just testing
+
+prefixes:
+  linkml: https://w3id.org/linkml/
+  schema: http://schema.org/
+
+imports:
+  - https://w3id.org/linkml/types
+
+classes:
+  A:
+    slots:
+      - id
+  B:
+    slots:
+      - id
+  C:
+    slots:
+      - id
+      - inlined_things
+      - inlined_as_list_things
+      - not_inlined_things
+slots:
+  id:
+    identifier: true
+  inlined_things:
+    inlined: true
+    multivalued: true
+    any_of:
+      - range: A
+      - range: B
+  inlined_as_list_things:
+    inlined_as_list: true
+    multivalued: true
+    any_of:
+      - range: A
+      - range: B
+  not_inlined_things:
+    multivalued: true
+    any_of:
+      - range: A
+      - range: B        
+        """
+        gen = PydanticGenerator(schema_str, package=PACKAGE)
+        code = gen.serialize()
+        # print(code)
+        lines = code.splitlines()
+        ix = lines.index("class C(ConfiguredBaseModel):")
+        assert lines[ix + 1] == "    inlined_things: Optional[Union[Dict[str,A],Dict[str,B]]] = Field(default_factory=list)"
+        assert lines[ix + 2] == "    inlined_as_list_things: Optional[List[Union[A, B]]] = Field(default_factory=list)"
+        assert lines[ix + 3] == "    not_inlined_things: Optional[List[str] = Field(default_factory=list)"
+
     def test_pydantic_inlining(self):
         #Case = namedtuple("multivalued", "inlined", "inlined_as_list", "B_has_identities")
         expected_default_factories = {
