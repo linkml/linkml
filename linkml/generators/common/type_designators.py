c5ca14b2965e73379e625c2499b525cab1f1a46e
Frank Dekervel <frank@kapernikov.com>
2023-02-21 22:27:44 +0100

    apply logic for type ancestors also for the default value (in addition to the accepted values)

--- linkml/generators/common/type_designators.py
+++ linkml/generators/common/type_designators.py
@@ -1,24 +1,29 @@
 from linkml_runtime.linkml_model.meta import SlotDefinition, ClassDefinition
 from linkml_runtime.utils.schemaview import SchemaView
 from linkml_runtime.utils.formatutils import camelcase
-from typing import List
+from typing import List, Tuple
 
 def get_type_designator_value(sv: SchemaView, type_designator_slot: SlotDefinition, class_def: ClassDefinition) -> str:
     """
         retuns the correct value for a type designator field for a given class, depending on its range
         this implements the logic described in https://github.com/linkml/linkml/issues/945:
     """
-    target_value = None
-    if type_designator_slot.range == 'uri':
-        target_value = sv.get_uri(class_def,expand=True,native=False)
-    elif type_designator_slot.range == 'string':
-        target_value = camelcase(class_def.name)
+    slot_types = set(sv.type_ancestors(type_designator_slot.range))
+    if 'uri' in slot_types:
+        return sv.get_uri(class_def,expand=True,native=False)
+    elif 'uriorcurie' in slot_types:
+        return sv.get_uri(class_def,expand=False, native=False)
+    elif 'string' in slot_types:
+        return camelcase(class_def.name)
     else:
-        target_value = sv.get_uri(class_def,expand=False, native=False)
-    return target_value
+        return sv.get_uri(class_def,expand=False, native=False)
 
 
 def get_accepted_type_designator_values(sv: SchemaView, type_designator_slot: SlotDefinition, class_def: ClassDefinition) -> List[str]:
+    """
+        retuns the accepted values for a type designator field for a given class, depending on its range
+        this implements the logic described in https://github.com/linkml/linkml/issues/945:
+    """
     accepted_uri_values = [
             sv.get_uri(class_def, expand=True, native=True),
             sv.get_uri(class_def, expand=True, native=False),
