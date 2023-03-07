ffaf4f71bec133373e34549ac6436d34ded0a901
Corey Cox <69321580+amc-corey-cox@users.noreply.github.com>
2023-03-07 12:50:26 -0800

    Re-enable tests for classes_modifying_slot

--- tests/test_generators/test_docgen.py
+++ tests/test_generators/test_docgen.py
@@ -7,7 +7,6 @@ import yaml
 from copy import copy
 from typing import List
 
-import pytest
 from linkml_runtime.utils.introspection import package_schemaview
 from linkml_runtime.utils.schemaview import SchemaView
 
@@ -368,14 +367,12 @@ class DocGeneratorTestCase(unittest.TestCase):
 
 
         # test that slots modifying classes are being rendered
-        pytest.skip(reason="TODO: why isn't this showing up?")
         assert_mdfile_contains(
             "type.md", "FamilialRelationship", after="## Slot Modifying Classes",
             followed_by=["## Properties",
                         "* Range"],
         )
 
-        pytest.skip(reason="TODO: why isn't this showing up?")
         assert_mdfile_contains(
             "type.md", "EmploymentEvent", after="## Slot Modifying Classes",
             followed_by=["## Properties",
