fe74980c4121575d9fad95239ab4e4f161418651
cmungall <cjm@berkeleybop.org>
2023-03-22 13:29:05 -0700

    pre-release of 1.5x series.
    Replaces #255

--- packages/linkml_runtime/linkml_model/extensions.py
+++ packages/linkml_runtime/linkml_model/extensions.py
@@ -1,5 +1,5 @@
 # Auto generated from extensions.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-07-13T02:59:55
+# Generation date: 2022-07-14T00:57:16
 # Schema: extensions
 #
 # id: https://w3id.org/linkml/extensions
