c325142f71a8301800387e446069d0d740577dc9
Kevin Schaper <kevinschaper@gmail.com>
2023-04-14 11:26:32 -0700

    Use a version of kitchen sink data that matches the schema exactly enough for pydantic's parsing

--- tests/linkml_runtime/test_loaders_dumpers/test_loaders_pydantic.py
+++ tests/linkml_runtime/test_loaders_dumpers/test_loaders_pydantic.py
@@ -19,10 +19,10 @@ class PydanticLoadersUnitTest(LoaderDumperTestCase):
         self.loader_test('book_series_lotr.json', BookSeries, json_loader)
 
     def test_yaml_loader_kitchen_sink(self):
-        self.loader_test('kitchen_sink_inst_01.yaml', Dataset, yaml_loader)
+        self.loader_test('kitchen_sink_normalized_inst_01.yaml', Dataset, yaml_loader)
 
     def test_json_loader_kitchen_sink(self):
-        self.loader_test('kitchen_sink_inst_01.json', Dataset, json_loader)
+        self.loader_test('kitchen_sink_normalized_inst_01.json', Dataset, json_loader)
 
 if __name__ == '__main__':
     unittest.main()
