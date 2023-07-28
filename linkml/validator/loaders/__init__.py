0d18b90cca3714ffe0a6ab9efe5bbdb1623c122c
Patrick Kalita <pkalita@lbl.gov>
2023-07-28 10:48:15 -0700

    Add CSV and TSV loaders

--- linkml/validator/loaders/__init__.py
+++ linkml/validator/loaders/__init__.py
@@ -1,4 +1,5 @@
+from linkml.validator.loaders.delimited_file_loader import CsvLoader, TsvLoader
 from linkml.validator.loaders.json_loader import JsonLoader
 from linkml.validator.loaders.loader import Loader
 
-__all__ = ["JsonLoader", "Loader"]
+__all__ = ["CsvLoader", "JsonLoader", "Loader", "TsvLoader"]
---
+++ tests/test_validator/test_delimited_file_loader.py
@@ -0,0 +1,69 @@
+import tempfile
+import unittest
+from contextlib import contextmanager
+
+from linkml.validator.loaders import CsvLoader, TsvLoader
+
+
+@contextmanager
+def data_file(test_data: str):
+    with tempfile.NamedTemporaryFile(mode="w+") as file:
+        file.write(test_data)
+        file.seek(0)
+        yield file.name
+
+
+class TestDelimitedFileLoader(unittest.TestCase):
+    def test_load(self):
+        for d, loader_cls in [(",", CsvLoader), ("\t", TsvLoader)]:
+            with self.subTest(loader=loader_cls):
+                data = f"""one{d} two{d} three{d} four
+1{d} 2.0001{d} three{d} 4.4.4
+"""
+                with data_file(data) as f:
+                    loader = loader_cls(f)
+                    instances = loader.iter_instances()
+                    self.assertEqual(
+                        next(instances),
+                        {"one": 1, "two": 2.0001, "three": "three", "four": "4.4.4"},
+                    )
+                    self.assertRaises(StopIteration, lambda: next(instances))
+
+    def test_load_empty_rows(self):
+        data = """one, two, three
+a, b, c
+,,
+d, e, f
+"""
+        with data_file(data) as csv_file:
+            loader = CsvLoader(csv_file)
+            instances = loader.iter_instances()
+            self.assertEqual(next(instances), {"one": "a", "two": "b", "three": "c"})
+            self.assertEqual(next(instances), {"one": "", "two": "", "three": ""})
+            self.assertEqual(next(instances), {"one": "d", "two": "e", "three": "f"})
+            self.assertRaises(StopIteration, lambda: next(instances))
+
+            loader = CsvLoader(csv_file, skip_empty_rows=True)
+            instances = loader.iter_instances()
+            self.assertEqual(next(instances), {"one": "a", "two": "b", "three": "c"})
+            self.assertEqual(next(instances), {"one": "d", "two": "e", "three": "f"})
+            self.assertRaises(StopIteration, lambda: next(instances))
+
+    def test_load_index_slot(self):
+        data = """one, two, three
+a, b, c
+d, e, f
+"""
+        with data_file(data) as csv_file:
+            loader = CsvLoader(csv_file, index_slot_name="some_things")
+            instances = loader.iter_instances()
+            self.assertEqual(
+                next(instances),
+                {
+                    "some_things": [
+                        {"one": "a", "two": "b", "three": "c"},
+                        {"one": "d", "two": "e", "three": "f"},
+                    ]
+                },
+            )
+            self.assertRaises(StopIteration, lambda: next(instances))
