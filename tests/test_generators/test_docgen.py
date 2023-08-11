2679d8dd76486da25f5e56df6e2ee93cb3c239cc
Sujay Patil <sujaysanjeev.patil@gmail.com>
2023-08-11 11:37:32 -0700

    fix black formatting in test_docgen

--- tests/test_generators/test_docgen.py
+++ tests/test_generators/test_docgen.py
@@ -218,21 +218,11 @@ class DocGeneratorTestCase(unittest.TestCase):
             followed_by=["## Identifier and Mapping Information", "### Schema Source"],
         )
 
-        assert_mdfile_contains(
-            "SubsetB.md",
-            "## Slots in subset",
-            after="### Schema Source"
-        )
+        assert_mdfile_contains("SubsetB.md", "## Slots in subset", after="### Schema Source")
 
-        assert_mdfile_does_not_contain(
-            "SubsetB.md",
-            "## Classes in subset"
-        )
+        assert_mdfile_does_not_contain("SubsetB.md", "## Classes in subset")
 
-        assert_mdfile_does_not_contain(
-            "SubsetB.md",
-            "## Enumerations in subset"
-        )
+        assert_mdfile_does_not_contain("SubsetB.md", "## Enumerations in subset")
 
         # test internal links
         assert_mdfile_contains("ceo.md", "Range: [Person](Person.md)", after="Properties")
