59c6edd5f26216ffb07e0e810a309f65149c18f3
Anja Strunk <anja.strunk@cloudandheat.com>
2023-08-29 14:59:23 +0200

    Reformat file with black

--- linkml/generators/shaclgen.py
+++ linkml/generators/shaclgen.py
@@ -118,7 +118,7 @@ class ShaclGenerator(Generator):
                     prop_pv(SH["class"], URIRef(range_ref))
                     if sv.get_identifier_slot(r) is not None:
                         prop_pv(SH.nodeKind, SH.IRI)
-                    #else:
+                    # else:
                     #    prop_pv(SH.nodeKind, SH.BlankNode)
                 elif r in sv.all_types().values():
                     rt = sv.get_type(r)
