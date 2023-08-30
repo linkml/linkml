edc3b3dea52613e13bfc903335e7e98867c42559
Anja Strunk <anja.strunk@cloudandheat.com>
2023-08-30 22:27:32 +0200

    Add any_of implementation

--- linkml/generators/shaclgen.py
+++ linkml/generators/shaclgen.py
@@ -1,7 +1,9 @@
 import logging
 import os
+from typing import Callable
 
 import click
+from linkml_runtime.linkml_model.meta import ElementName
 from linkml_runtime.utils.formatutils import underscore
 from linkml_runtime.utils.schemaview import SchemaView
 from rdflib import BNode, Graph, Literal, URIRef
@@ -104,53 +106,115 @@ class ShaclGenerator(Generator):
                     prop_pv_literal(SH.minCount, 1)
                 prop_pv_literal(SH.minInclusive, s.minimum_value)
                 prop_pv_literal(SH.maxInclusive, s.maximum_value)
-                r = s.range
-                if r in sv.all_classes():
-                    range_ref = sv.get_uri(r, expand=True)
-                    prop_pv(SH["class"], URIRef(range_ref))
-                    if sv.get_identifier_slot(r) is not None:
-                        prop_pv(SH.nodeKind, SH.IRI)
-                    else:
-                        prop_pv(SH.nodeKind, SH.BlankNode)
-                elif r in sv.all_types().values():
-                    rt = sv.get_type(r)
-                    if rt.uri:
-                        prop_pv(SH.datatype, rt.uri)
-                    else:
-                        logging.error(f"No URI for type {rt.name}")
-                elif r in sv.all_enums():
-                    e = sv.get_enum(r)
-                    pv_node = BNode()
-                    Collection(
-                        g,
-                        pv_node,
-                        [
-                            URIRef(sv.expand_curie(pv.meaning)) if pv.meaning else Literal(pv_name)
-                            for pv_name, pv in e.permissible_values.items()
-                        ],
-                    )
-                    prop_pv(SH["in"], pv_node)
-                else:
-                    if r == "string":
-                        prop_pv(SH.datatype, LINK_ML_TYPES_STRING)
-                    elif r == "boolean":
-                        prop_pv(SH.datatype, LINK_ML_TYPES_BOOL)
-                    elif r == "duration":
-                        prop_pv(SH.datatype, LINK_ML_TYPES_DURATION)
-                    elif r == "datetime":
-                        prop_pv(SH.datatype, LINK_ML_TYPES_DATETIME)
-                    elif r == "date":
-                        prop_pv(SH.datatype, LINK_ML_TYPES_DATE)
-                    elif r == "time":
-                        prop_pv(SH.datatype, LINK_ML_TYPES_TIME)
-                    elif r == "decimal":
-                        prop_pv(SH.datatype, LINK_ML_TYPES_DECIMAL)
-                    elif r == "integer":
-                        prop_pv(SH.datatype, LINK_ML_TYPES_INTEGER)
-                if s.pattern:
-                    prop_pv(SH.pattern, Literal(s.pattern))
 
+                all_classes = sv.all_classes()
+                if s.any_of:
+                    or_node = BNode()
+                    prop_pv(SH["or"], or_node)
+                    range_list = []
+                    for any in s.any_of:
+                        r = any.range
+                        if r in all_classes:
+                            class_node = BNode()
+
+                            def cl_node_pv(p, v):
+                                if v is not None:
+                                    g.add((class_node, p, v))
+
+                            self._add_class(cl_node_pv, r)
+                            range_list.append(class_node)
+                        elif r in sv.all_types().values():
+                            t_node = BNode()
+
+                            def t_node_pv(p, v):
+                                if v is not None:
+                                    g.add((t_node, p, v))
+
+                            self._add_type(t_node_pv, r)
+                            range_list.append(t_node)
+                        elif r in sv.all_enums():
+                            en_node = BNode()
+
+                            def en_node_pv(p, v):
+                                if v is not None:
+                                    g.add((en_node, p, v))
+
+                            self._add_enum(g, en_node_pv, r)
+                            range_list.append(en_node)
+                        else:
+                            st_node = BNode()
+
+                            def st_node_pv(p, v):
+                                if v is not None:
+                                    g.add((st_node, p, v))
+
+                            add_simple_data_type(st_node_pv, r)
+                            range_list.append(st_node)
+                    Collection(g, or_node, range_list)
+
+                else:
+                    r = s.range
+                    if r in all_classes:
+                        self._add_class(prop_pv, r)
+                        if sv.get_identifier_slot(r) is not None:
+                            prop_pv(SH.nodeKind, SH.IRI)
+                        else:
+                            prop_pv(SH.nodeKind, SH.BlankNode)
+                    elif r in sv.all_types().values():
+                        self._add_type(prop_pv, r)
+                    elif r in sv.all_enums():
+                        self._add_enum(g, prop_pv, r)
+                    else:
+                        add_simple_data_type(prop_pv, r)
+                    if s.pattern:
+                        prop_pv(SH.pattern, Literal(s.pattern))
         return g
+
+    def _add_class(self, func: Callable, r: ElementName) -> None:
+        sv = self.schemaview
+        range_ref = sv.get_uri(r, expand=True)
+        func(SH["class"], URIRef(range_ref))
+
+    def _add_enum(self, g: Graph, func: Callable, r: ElementName) -> None:
+        sv = self.schemaview
+        enum = sv.get_enum(r)
+        pv_node = BNode()
+        Collection(
+            g,
+            pv_node,
+            [
+                URIRef(sv.expand_curie(pv.meaning)) if pv.meaning else Literal(pv_name)
+                for pv_name, pv in enum.permissible_values.items()
+            ],
+        )
+        func(SH["in"], pv_node)
+
+    def _add_type(self, func: Callable, r: ElementName) -> None:
+        sv = self.schemaview
+        rt = sv.get_type(r)
+        if rt.uri:
+            func(SH.datatype, rt.uri)
+        else:
+            logging.error(f"No URI for type {rt.name}")
+
+
+def add_simple_data_type(func: Callable, range: ElementName) -> None:
+    if range == "string":
+        func(SH.datatype, LINK_ML_TYPES_STRING)
+    elif range == "boolean":
+        func(SH.datatype, LINK_ML_TYPES_BOOL)
+    elif range == "duration":
+        func(SH.datatype, LINK_ML_TYPES_DURATION)
+    elif range == "datetime":
+        func(SH.datatype, LINK_ML_TYPES_DATETIME)
+    elif range == "date":
+        func(SH.datatype, LINK_ML_TYPES_DATE)
+    elif range == "time":
+        func(SH.datatype, LINK_ML_TYPES_TIME)
+    elif range == "decimal":
+        func(SH.datatype, LINK_ML_TYPES_DECIMAL)
+    elif range == "integer":
+        func(SH.datatype, LINK_ML_TYPES_INTEGER)
 
 
 @shared_arguments(ShaclGenerator)
