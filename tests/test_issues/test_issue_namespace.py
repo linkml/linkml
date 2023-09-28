41f58f31c56299e9ffd6857b42a720acf5b92a52
cmungall <cjm@berkeleybop.org>
2023-09-27 18:18:10 -0700

    skipping test_issue_namespace.py, see https://github.com/linkml/linkml/issues/1650

--- tests/test_issues/test_issue_namespace.py
+++ tests/test_issues/test_issue_namespace.py
@@ -1,8 +1,12 @@
+import pytest
+
 from linkml.generators.rdfgen import RDFGenerator
 
 
 # TODO: Find out why test_issue_namespace is emitting generation_date in the TYPE namespace
+@pytest.mark.skip(reason="non-deterministic, see https://github.com/linkml/linkml/issues/1650")
 def test_namespace(input_path, snapshot):
+    # TODO: Do not depend on an external URL whose content may change
     context = "https://biolink.github.io/biolink-model/context.jsonld"
     output = RDFGenerator(input_path("issue_namespace.yaml")).serialize(context=context)
     assert output == snapshot("issue_namespace.ttl")
