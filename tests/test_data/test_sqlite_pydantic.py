1d6f6314d500ca0c72865ecc386215f5ca45d1f1
Patrick Kalita <pkalita@lbl.gov>
2023-10-05 16:46:40 -0700

    Test fixes for Pydantic 1 & 2 compatibility

--- tests/test_data/test_sqlite_pydantic.py
+++ tests/test_data/test_sqlite_pydantic.py
@@ -5,15 +5,9 @@ import yaml
 from linkml_runtime.dumpers import yaml_dumper
 from sqlalchemy.orm import sessionmaker
 
-import tests.test_data.model.personinfo
+from linkml.generators.pydanticgen import PydanticGenerator
 from linkml.utils.sqlutils import SQLStore
 from tests.test_data.environment import env
-from tests.test_data.model.personinfo_pydantic import (
-    Container,
-    FamilialRelationship,
-    GenderType,
-    Person,
-)
 from tests.utils.dict_comparator import compare_yaml
 
 SCHEMA = env.input_path("personinfo.yaml")
@@ -34,15 +28,20 @@ class SQLiteStoreTest(unittest.TestCase):
     - :meth:`SQLStore.load`
     """
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.personinfo_module = PydanticGenerator(SCHEMA).compile_module()
+
     def test_enums(self):
         """
         Tests that enum objects can be constructed inlined.
 
         See https://github.com/linkml/linkml/issues/817
         """
-        FamilialRelationship(type="SIBLING_OF", related_to="x")
-        p = Person(id="x", gender=GenderType("cisgender_man"))
-        self.assertIsInstance(p.gender, GenderType)
+        mod = self.personinfo_module
+        mod.FamilialRelationship(type="SIBLING_OF", related_to="x")
+        p = mod.Person(id="x", gender=mod.GenderType("cisgender_man"))
+        self.assertIsInstance(p.gender, str)
 
     def test_sqlite_store(self):
         """
@@ -51,14 +50,15 @@ class SQLiteStoreTest(unittest.TestCase):
         # step 1: setup
         # TODO: currently it is necessary to pass the actual yaml rather than a schema object
         # endpoint = SQLiteEndpoint(sv.schema, database_path=DB, include_schema_in_database=False)
+        mod = self.personinfo_module
         endpoint = SQLStore(SCHEMA, database_path=DB, include_schema_in_database=False)
-        endpoint.native_module = tests.test_data.model.personinfo_pydantic
+        endpoint.native_module = mod
         endpoint.db_exists(force=True)
         # step 2: load data from file and store in SQLStore
         # TODO: use yaml_loader once this supports pydantic
         with open(DATA) as file:
             dict_obj = yaml.safe_load(file)
-        container = Container(**dict_obj)
+        container = mod.Container(**dict_obj)
         endpoint.dump(container)
         # step 3: test query using SQL Alchemy
         session_class = sessionmaker(bind=endpoint.engine)
@@ -69,7 +69,7 @@ class SQLiteStoreTest(unittest.TestCase):
         q = session.query(endpoint.module.FamilialRelationship)
         # step 4: test loading from SQLStore
         # 4a: first test load_all, diff to original data should be empty
-        [returned_container] = endpoint.load_all(target_class=Container)
+        [returned_container] = endpoint.load_all(target_class=mod.Container)
         # y = yaml_dumper.dumps(x[0])
         returned_dict = returned_container.dict(exclude_none=True)
         if False:
