b572a1fb9ed94f7356c8dec1493230518e3fbaaf
Patrick Kalita <pkalita@lbl.gov>
2023-10-06 09:54:55 -0700

    Add option to JsonschemaValidationPlugin to provide pre-generated JSON Schema

--- linkml/validator/plugins/jsonschema_validation_plugin.py
+++ linkml/validator/plugins/jsonschema_validation_plugin.py
@@ -1,4 +1,5 @@
-from typing import Any, Iterator
+import os
+from typing import Any, Iterator, Optional
 
 import jsonschema
 from jsonschema.exceptions import best_match
@@ -11,16 +12,25 @@ from linkml.validator.validation_context import ValidationContext
 class JsonschemaValidationPlugin(ValidationPlugin):
     """A validation plugin which validates instances using a JSON Schema validator."""
 
-    def __init__(self, closed: bool = False, strict: bool = False) -> None:
+    def __init__(
+        self,
+        closed: bool = False,
+        strict: bool = False,
+        json_schema_path: Optional[os.PathLike] = None,
+    ) -> None:
         """Constructor method
 
         :param closed: If True, additional properties are not allowed on instances.
             Defaults to False.
         :param strict: If true, stop validating after the first validation problem
             is found. Defaults to False.
+        :param json_schema_path: If provided, JSON Schema will not be generated from the schema,
+            instead it will be read from this path. In this case the value of the `closed` argument
+            is disregarded and the open- or closed-ness of the existing JSON Schema is taken as-is.
         """
         self.closed = closed
         self.strict = strict
+        self.json_schema_path = json_schema_path
 
     def process(self, instance: Any, context: ValidationContext) -> Iterator[ValidationResult]:
         """Perform JSON Schema validation on the provided instance
@@ -30,7 +40,7 @@ class JsonschemaValidationPlugin(ValidationPlugin):
         :return: Iterator over validation results
         :rtype: Iterator[ValidationResult]
         """
-        json_schema = context.json_schema(closed=self.closed)
+        json_schema = context.json_schema(closed=self.closed, path_override=self.json_schema_path)
         validator = jsonschema.Draft7Validator(
             json_schema, format_checker=jsonschema.Draft7Validator.FORMAT_CHECKER
         )
