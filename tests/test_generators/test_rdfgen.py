113e7f9c7059f986a59a4b14961c5fa2cb208a36
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-01-05 15:42:51 -0800

    add test to confirm that example in ticket returns two annotations, each with a tag/value pair

--- tests/test_generators/test_rdfgen.py
+++ tests/test_generators/test_rdfgen.py
@@ -1,7 +1,34 @@
 import pytest
+import rdflib
 from rdflib import Graph
-
+from pprint import pprint
 from linkml.generators.rdfgen import RDFGenerator
+
+schema = """
+id: http://example.org/interval
+
+default_curi_maps:
+  - semweb_context
+
+prefixes:
+  ex: http://example.org/
+  schema: http://schema.org/
+  linkml: https://w3id.org/linkml/
+
+imports:
+  - linkml:types
+
+default_prefix: ex
+
+classes:
+
+  c:
+    annotations:
+      - tag: my_tag1
+        value: my_value1
+      - tag: my_tag2
+        value: my_value2
+"""
 
 JSONLD = """
 {
@@ -22,13 +49,40 @@ JSONLD = """
   ]
 }"""
 
+def test_annotation_extensions():
+    """Test that annotation extensions are properly serialized"""
+    s = RDFGenerator(schema, mergeimports=False).serialize()
+    rdf_graph = Graph()
+    rdf_graph.parse(data=s, format='turtle')
+
+    # Query for annotations in the ClassDefinition
+    query = """
+        SELECT ?example ?tag
+        WHERE {
+            ex:C linkml:annotations ?annotation .
+            ?annotation skos:example ?example .
+            ?annotation linkml:tag ?tag .
+        }
+        """
+
+    results = list(rdf_graph.query(query))
+
+    # Check if there are exactly two annotations
+    assert len(results) == 2
+
+    # Check each annotation for the required properties
+    for example, tag in results:
+        assert isinstance(example, rdflib.Literal)
+        assert isinstance(tag, rdflib.URIRef)
 
 @pytest.mark.skip("TODO")
 def test_rdfgen(kitchen_sink_path):
     """rdf"""
     s = RDFGenerator(kitchen_sink_path, mergeimports=False).serialize()
     g = Graph()
-    g.parse(data=s, format="turtle")
+    g.parse(data=s)
+    pprint(s)
+
 
 
 @pytest.mark.skip("TODO")
