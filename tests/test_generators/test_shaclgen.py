16f20d90c5cfba265e837eda5dafb4e225c40e1c
Anja Strunk <anja.strunk@cloudandheat.com>
2024-02-29 08:53:49 +0100

    reformat test_shacl

--- tests/test_generators/test_shaclgen.py
+++ tests/test_generators/test_shaclgen.py
@@ -20,7 +20,7 @@ EXPECTED = [
 
 EXPECTED_closed = [
     (
-        rdflib.term.URIRef('https://w3id.org/linkml/tests/kitchen_sink/Person'),
+        rdflib.term.URIRef("https://w3id.org/linkml/tests/kitchen_sink/Person"),
         rdflib.term.URIRef("http://www.w3.org/ns/shacl#closed"),
         rdflib.term.Literal("false", datatype=rdflib.term.URIRef("http://www.w3.org/2001/XMLSchema#boolean")),
     ),
@@ -28,44 +28,81 @@ EXPECTED_closed = [
 
 EXPECTED_any_of = [
     (
-        rdflib.term.URIRef('https://w3id.org/linkml/tests/kitchen_sink/AnyOfSimpleType'),
-        [(rdflib.term.URIRef('http://www.w3.org/ns/shacl#datatype'),
-          rdflib.term.URIRef('http://www.w3.org/2001/XMLSchema#integer')),
-         (rdflib.term.URIRef('http://www.w3.org/ns/shacl#datatype'),
-          rdflib.term.URIRef('http://www.w3.org/2001/XMLSchema#string'))]
+        rdflib.term.URIRef("https://w3id.org/linkml/tests/kitchen_sink/AnyOfSimpleType"),
+        [
+            (
+                rdflib.term.URIRef("http://www.w3.org/ns/shacl#datatype"),
+                rdflib.term.URIRef("http://www.w3.org/2001/XMLSchema#integer"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/ns/shacl#datatype"),
+                rdflib.term.URIRef("http://www.w3.org/2001/XMLSchema#string"),
+            ),
+        ],
     ),
     (
-        rdflib.term.URIRef('https://w3id.org/linkml/tests/kitchen_sink/AnyOfClasses'),
-        [(rdflib.term.URIRef('http://www.w3.org/ns/shacl#class'),
-          rdflib.term.URIRef('https://w3id.org/linkml/tests/kitchen_sink/Person')),
-         (rdflib.term.URIRef('http://www.w3.org/ns/shacl#class'),
-          rdflib.term.URIRef('https://w3id.org/linkml/tests/kitchen_sink/Organization'))]
+        rdflib.term.URIRef("https://w3id.org/linkml/tests/kitchen_sink/AnyOfClasses"),
+        [
+            (
+                rdflib.term.URIRef("http://www.w3.org/ns/shacl#class"),
+                rdflib.term.URIRef("https://w3id.org/linkml/tests/kitchen_sink/Person"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/ns/shacl#class"),
+                rdflib.term.URIRef("https://w3id.org/linkml/tests/kitchen_sink/Organization"),
+            ),
+        ],
     ),
     (
-        rdflib.term.URIRef('https://w3id.org/linkml/tests/kitchen_sink/AnyOfEnums'),
-        [(rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-          rdflib.term.URIRef('https://example.org/bizcodes/001')),
-         (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-          rdflib.term.URIRef('https://example.org/bizcodes/002')),
-         (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-          rdflib.term.URIRef('https://example.org/bizcodes/003')),
-         (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-          rdflib.term.URIRef('https://example.org/bizcodes/004')),
-         (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'), rdflib.term.Literal('TODO'))]
+        rdflib.term.URIRef("https://w3id.org/linkml/tests/kitchen_sink/AnyOfEnums"),
+        [
+            (
+                rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"),
+                rdflib.term.URIRef("https://example.org/bizcodes/001"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"),
+                rdflib.term.URIRef("https://example.org/bizcodes/002"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"),
+                rdflib.term.URIRef("https://example.org/bizcodes/003"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"),
+                rdflib.term.URIRef("https://example.org/bizcodes/004"),
+            ),
+            (rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"), rdflib.term.Literal("TODO")),
+        ],
     ),
     (
-        rdflib.term.URIRef('https://w3id.org/linkml/tests/kitchen_sink/AnyOfMix'),
-        [(rdflib.term.URIRef('http://www.w3.org/ns/shacl#datatype'),rdflib.term.URIRef('http://www.w3.org/2001/XMLSchema#integer')),
-          (rdflib.term.URIRef('http://www.w3.org/ns/shacl#class'),
-          rdflib.term.URIRef('https://w3id.org/linkml/tests/kitchen_sink/Person')),
-         (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-          rdflib.term.URIRef('https://example.org/bizcodes/001')),
-         (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-          rdflib.term.URIRef('https://example.org/bizcodes/002')),
-         (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-          rdflib.term.URIRef('https://example.org/bizcodes/003')),
-         (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'),
-          rdflib.term.URIRef('https://example.org/bizcodes/004'))]
+        rdflib.term.URIRef("https://w3id.org/linkml/tests/kitchen_sink/AnyOfMix"),
+        [
+            (
+                rdflib.term.URIRef("http://www.w3.org/ns/shacl#datatype"),
+                rdflib.term.URIRef("http://www.w3.org/2001/XMLSchema#integer"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/ns/shacl#class"),
+                rdflib.term.URIRef("https://w3id.org/linkml/tests/kitchen_sink/Person"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"),
+                rdflib.term.URIRef("https://example.org/bizcodes/001"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"),
+                rdflib.term.URIRef("https://example.org/bizcodes/002"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"),
+                rdflib.term.URIRef("https://example.org/bizcodes/003"),
+            ),
+            (
+                rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"),
+                rdflib.term.URIRef("https://example.org/bizcodes/004"),
+            ),
+        ],
     ),
 ]
 
@@ -74,6 +111,7 @@ def test_shacl(kitchen_sink_path):
     """tests shacl generation"""
     shaclstr = ShaclGenerator(kitchen_sink_path, mergeimports=True).serialize()
     do_test(shaclstr, EXPECTED)
+
 
 def test_shacl_closed(kitchen_sink_path):
     """tests shacl generation"""
@@ -91,18 +129,22 @@ def do_test(shaclstr, expected):
 
     assert_any_of(EXPECTED_any_of, triples)
 
-def assert_any_of(expected: List[Tuple[rdflib.term.URIRef, List[Tuple[rdflib.term.URIRef, rdflib.term.URIRef]]]],
-                  triples: List) -> None:
+
+def assert_any_of(
+    expected: List[Tuple[rdflib.term.URIRef, List[Tuple[rdflib.term.URIRef, rdflib.term.URIRef]]]], triples: List
+) -> None:
     for ex in expected:
         found = False
         for property_triple in triples:
             # look for "property" triplet
-            if property_triple[0] == ex[0] and \
-                    property_triple[1] == rdflib.term.URIRef('http://www.w3.org/ns/shacl#property'):
+            if property_triple[0] == ex[0] and property_triple[1] == rdflib.term.URIRef(
+                "http://www.w3.org/ns/shacl#property"
+            ):
                 # look for "or" triplet
                 for or_triplet in triples:
                     if or_triplet[0] == property_triple[2] and or_triplet[1] == rdflib.term.URIRef(
-                            'http://www.w3.org/ns/shacl#or'):
+                        "http://www.w3.org/ns/shacl#or"
+                    ):
                         found = True
                         assert Counter(_get_data_type(or_triplet[2], triples)) == Counter(ex[1])
         if not found:
@@ -123,34 +165,33 @@ def _get_data_type(blank_node: rdflib.term.BNode, triples: List) -> List[rdflib.
     for node_triplet in triples:
         if node_triplet[0] == blank_node:
             # look for first node
-            if node_triplet[1] == rdflib.term.URIRef(
-                    'http://www.w3.org/1999/02/22-rdf-syntax-ns#first'):
+            if node_triplet[1] == rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"):
                 # parsing first rdf triples of list
                 if isinstance(node_triplet[2], rdflib.Literal):
                     # we found a leaf as first node
                     datatypes.append(
-                        (rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#first'), node_triplet[2]))
+                        (rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#first"), node_triplet[2])
+                    )
                 elif isinstance(node_triplet[2], rdflib.BNode):
                     # we found a blank node and have to retrieve all triples, which have blank node as origin
                     datatypes.extend(_get_data_type(node_triplet[2], triples))
                 elif isinstance(node_triplet[2], rdflib.term.URIRef):
                     # we found a URI as first node
-                    if node_triplet[1] == rdflib.term.URIRef('http://www.w3.org/ns/shacl#in'):
+                    if node_triplet[1] == rdflib.term.URIRef("http://www.w3.org/ns/shacl#in"):
                         # we found an enumeration
                         datatypes.extend(_get_data_type(node_triplet[2], triples))
                     else:
                         datatypes.append((node_triplet[1], node_triplet[2]))
-            elif node_triplet[1] == rdflib.term.URIRef('http://www.w3.org/ns/shacl#in'):
+            elif node_triplet[1] == rdflib.term.URIRef("http://www.w3.org/ns/shacl#in"):
                 # we found an enumeration
                 datatypes.extend(_get_data_type(node_triplet[2], triples))
-            elif node_triplet[1] == rdflib.term.URIRef('http://www.w3.org/ns/shacl#datatype'):
+            elif node_triplet[1] == rdflib.term.URIRef("http://www.w3.org/ns/shacl#datatype"):
                 # we found a data type
                 datatypes.append((node_triplet[1], node_triplet[2]))
-            elif node_triplet[1] == rdflib.term.URIRef('http://www.w3.org/ns/shacl#class'):
+            elif node_triplet[1] == rdflib.term.URIRef("http://www.w3.org/ns/shacl#class"):
                 # we found a data type
                 datatypes.append((node_triplet[1], node_triplet[2]))
             # look for remaining rdf triples in list
-            elif node_triplet[1] == rdflib.term.URIRef(
-                    'http://www.w3.org/1999/02/22-rdf-syntax-ns#rest'):
+            elif node_triplet[1] == rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#rest"):
                 datatypes.extend(_get_data_type(node_triplet[2], triples))
     return datatypes
