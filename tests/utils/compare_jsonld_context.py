a4c8b24d3671fffb9d3c35c1cb33fdf1cfebaa34
Vincent Kelleher <vincent.kelleher@gaia-x.eu>
2024-03-14 17:23:31 +0100

    fix JSON-LD Context generator not generating classes/slots when default_namespace is set
    Closes #1881
    
    Signed-off-by: Vincent Kelleher <vincent.kelleher@gaia-x.eu>

---
+++ tests/utils/compare_jsonld_context.py
@@ -0,0 +1,16 @@
+import json
+from pathlib import Path
+
+
+class CompareJsonldContext:
+
+    @staticmethod
+    def compare_with_snapshot(jsonld_context: str, snapshot: Path):
+        with open(snapshot, "r") as snapshot:
+            actual = json.loads(jsonld_context)
+            expected = json.loads(snapshot.read())
+
+            del actual["comments"]["generation_date"]
+            del expected["comments"]["generation_date"]
+
+            assert actual == expected
