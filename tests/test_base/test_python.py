ecd76c2f4c70d35f83125731c193690dde0615e5
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-03-15 15:16:33 -0700

    convet test_python to snapshot fixture

--- tests/test_base/test_python.py
+++ tests/test_base/test_python.py
@@ -1,74 +1,15 @@
-import unittest
+import pytest
+from pathlib import Path
 
-from linkml import LOCAL_ANNOTATIONS_YAML_FILE, LOCAL_EXTENSIONS_YAML_FILE
+from linkml import LOCAL_MODEL_YAML_FILES
 from linkml.generators.pythongen import PythonGenerator
-from tests.test_base.environment import env
-from tests.utils.filters import metadata_filter
-from tests.utils.python_comparator import compare_python
-from tests.utils.test_environment import TestEnvironmentTestCase
+from linkml_runtime.utils.compile_python import compile_python
 
 
-class PythonTestCase(TestEnvironmentTestCase):
-    """Generate python for all of the models, compare them against what has been published
-    and verify that they compile"""
-
-    env = env
-
-    def test_types_python(self):
-        """Build includes/types.py"""
-        env.generate_single_file(
-            "types.py",
-            lambda: PythonGenerator(env.types_yaml, importmap=env.import_map, genmeta=True).serialize(),
-            value_is_returned=True,
-            filtr=metadata_filter,
-            comparator=lambda exp, act: compare_python(exp, act, self.env.expected_path("types.py")),
-            use_testing_root=True,
-        )
-
-    def test_mapping_python(self):
-        """Build includes/mappings.py"""
-        env.generate_single_file(
-            "mappings.py",
-            lambda: PythonGenerator(env.mapping_yaml, importmap=env.import_map, genmeta=True).serialize(),
-            value_is_returned=True,
-            filtr=metadata_filter,
-            comparator=lambda exp, act: compare_python(exp, act, self.env.expected_path("mappings.py")),
-            use_testing_root=True,
-        )
-
-    def test_extensions_python(self):
-        """Build includes/extensions.py"""
-        env.generate_single_file(
-            "extensions.py",
-            lambda: PythonGenerator(LOCAL_EXTENSIONS_YAML_FILE, importmap=env.import_map, genmeta=True).serialize(),
-            value_is_returned=True,
-            filtr=metadata_filter,
-            comparator=lambda exp, act: compare_python(exp, act, self.env.expected_path("extensions.py")),
-            use_testing_root=True,
-        )
-
-    def test_annotations_python(self):
-        """Build includes/annotations.py"""
-        env.generate_single_file(
-            "annotations.py",
-            lambda: PythonGenerator(LOCAL_ANNOTATIONS_YAML_FILE, importmap=env.import_map, genmeta=True).serialize(),
-            value_is_returned=True,
-            filtr=metadata_filter,
-            comparator=lambda exp, act: compare_python(exp, act, self.env.expected_path("annotations.py")),
-            use_testing_root=True,
-        )
-
-    def test_metamodel_python(self):
-        """Build meta.py"""
-        env.generate_single_file(
-            "meta.py",
-            lambda: PythonGenerator(env.meta_yaml, importmap=env.import_map, genmeta=True).serialize(),
-            value_is_returned=True,
-            filtr=metadata_filter,
-            comparator=lambda exp, act: compare_python(exp, act, self.env.expected_path("meta.py")),
-            use_testing_root=True,
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("model", LOCAL_MODEL_YAML_FILES)
+def test_models_python(model, snapshot):
+    generated = PythonGenerator(model).serialize()
+    # ensure valid python
+    compile_python(generated, "test")
+    output_file = Path(model).with_suffix(".py").name
+    assert generated == snapshot(output_file)
