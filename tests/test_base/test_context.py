a8f7d5ae0e8e514f1a8a47462473ae47a49f92ff
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-03-15 18:04:54 -0700

    split off metamodel tests

--- tests/test_base/test_context.py
+++ tests/test_base/test_context.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 import pytest
 
-from linkml import (
-    LOCAL_MODEL_YAML_FILES,
-    NAMESPACES,
+from tests import (
+    LOCAL_MODEL_YAML_NO_META,
+    NAMESPACES_NO_META,
 )
 from linkml.generators.jsonldcontextgen import ContextGenerator
 
 
-@pytest.mark.parametrize("model,namespace", zip(LOCAL_MODEL_YAML_FILES, NAMESPACES))
+@pytest.mark.parametrize("model,namespace", zip(LOCAL_MODEL_YAML_NO_META, NAMESPACES_NO_META))
 def test_models_jsonld_context(model, namespace, snapshot):
     generated = ContextGenerator(model).serialize(base=namespace)
     output_file = Path(model).with_suffix(".jsonld").name
--- tests/test_base/test_python.py
+++ tests/test_base/test_python.py
@@ -3,11 +3,11 @@ from pathlib import Path
 import pytest
 from linkml_runtime.utils.compile_python import compile_python
 
-from linkml import LOCAL_MODEL_YAML_FILES
+from tests import LOCAL_MODEL_YAML_NO_META
 from linkml.generators.pythongen import PythonGenerator
 
 
-@pytest.mark.parametrize("model", LOCAL_MODEL_YAML_FILES)
+@pytest.mark.parametrize("model", LOCAL_MODEL_YAML_NO_META)
 def test_models_python(model, snapshot):
     generated = PythonGenerator(model).serialize()
     # ensure valid python
