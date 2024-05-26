1f66d5719d2f9dd4a2ad5de1bf05484b6b444b7a
Vlad Korolev <vlad@v-lad.org>
2024-05-26 15:37:30 -0400

    Make quality checks happy (#2136)

--- tests/test_compliance/test_core_compliance.py
+++ tests/test_compliance/test_core_compliance.py
@@ -2,9 +2,9 @@
 
 import sys
 import unicodedata
+from _decimal import Decimal
 
 import pytest
-from _decimal import Decimal
 from linkml_runtime.utils.formatutils import underscore
 from pydantic.version import VERSION as PYDANTIC_VERSION
 
