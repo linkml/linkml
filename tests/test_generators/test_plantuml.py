556dd1ef5e25ebf41f10b375ae7c6ac33c9eb2e6
Jim Snyder <39277823+jsnyder-csdisco@users.noreply.github.com>
2024-07-03 18:01:19 -0500

    Fix plantUML syntax bugs to do spacing and add suppor for SVG tooltips (#2161)
    * Fix plant uml syntax bugs to do spacing and add suppor for SVG hovertext
    
    * Fix failing tests that use PlantUML strings.
    
    * Fix lint failures.
    
    * Remove quotes and [opt] and [req] from PlantUML structures for fields
    
    * Fix quoting per the linter check

--- tests/test_generators/test_plantuml.py
+++ tests/test_generators/test_plantuml.py
@@ -16,14 +16,14 @@ MARKDOWN_FOOTER = """
 """
 
 PERSON = """
-class "Person" {
-    {field} id: string [req]
-    {field} name: string [opt]
-    {field} age_in_years: integer [opt]
-    {field} species_name: string [opt]
-    {field} stomach_count: integer [opt]
-    {field} is_living: LifeStatusEnum [opt]
-    {field} aliases: string [0..*]
+class "Person" [[{A person, living or dead}]] {
+    {field} id : string  
+    {field} name : string  
+    {field} age_in_years : integer  
+    {field} species_name : string  
+    {field} stomach_count : integer  
+    {field} is_living : LifeStatusEnum  
+    {field} aliases : string  [0..*]
 }
 """
 
