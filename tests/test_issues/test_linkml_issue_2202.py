ea302d2d1bcbb5e08ab4b808f60f14eb00a90c7f
Mark A. Miller <MAM@lbl.gov>
2024-07-15 09:39:58 -0400

    testing relationships now

--- tests/test_issues/test_linkml_issue_2202.py
+++ tests/test_issues/test_linkml_issue_2202.py
@@ -6,9 +6,8 @@ from linkml_runtime.loaders import YAMLLoader
 from linkml.generators import PythonGenerator
 from linkml.validator import ValidationReport, Validator
 
-# THIS IS EXPERIMENTATION FOR CHECKINS
+# THIS IS EXPERIMENTATION FOR CHECKING
 # # SOME OF NMDC'S VALIDATION EXCEPTIONS
-# ASSUMPTIONS ABOUT
 
 minimal_household_schema = """
 name: minimal_household_schema
@@ -16,6 +15,7 @@ id: http://example.com/minimal_household_schema
 
 prefixes:
   linkml: https://w3id.org/linkml/
+  minhouse: http://example.com/minimal_household_schema/
     
 imports:
   - linkml:types
@@ -28,6 +28,16 @@ classes:
     is_a: NamedThing
     slots:
       - name
+      - has_relative
+      - has_pet
+  Pet:
+    is_a: NamedThing
+    slots:
+      - name
+  Database:
+    slots:
+      - people
+      - pets
   
 slots:
   id:
@@ -36,15 +46,33 @@ slots:
   name:
     range: string
     required: true
-  has_roommate: {}
+  has_relative:
+    range: Person
   has_pet:
+    range: Pet
+  people:
+    range: Person
+    multivalued: true
+    inlined_as_list: true
+  pets:
+    range: Pet
+    multivalued: true
+    inlined_as_list: true    
   
 """
 # todo boilerplate in fixture (could return schemabuidler)
 #  each method can use fixture and modify with schemabuilder
 
-minimal_household_data = """
-id: http://example.com/minimal_household_schema/1
+minimal_household_database = """
+people:
+  - id: minhouse:1
+    name: Superman
+    has_pet: minhouse:2
+  - id: minhouse:3
+    name: Batman  
+pets:
+  - id: minhouse:2
+    name: Krypto    
 """
 
 minimal_view = SchemaView(minimal_household_schema)
@@ -52,13 +80,13 @@ minimal_view = SchemaView(minimal_household_schema)
 validator_for_minimal = Validator(minimal_view.schema)
 
 
-def test_minimal_view():
-    assert minimal_view is not None
+def test_view_created():
+    assert type(minimal_view).__name__ == "SchemaView"
 
 
 def test_minimal_validation():
     report_from_minimal_validation: ValidationReport = validator_for_minimal.validate(
-        minimal_household_data, "NamedThing"
+        minimal_household_database, "Database"
     )
 
     assert len(report_from_minimal_validation.results) == 0
@@ -76,10 +104,9 @@ def test_convert_to_rdf():
     # Execute the code in the newly created module's namespace
     exec(generated_python, minimal_household_module.__dict__)  # todo: doesn't linkml have a compile python method?
 
-    # Now you can access the class NamedThing from dynamic_module
-    NamedThing = getattr(minimal_household_module, "NamedThing")
+    Database = getattr(minimal_household_module, "Database")
 
     my_loader = YAMLLoader()
-    minimal_instance = my_loader.load(source=minimal_household_data, target_class=NamedThing, schema_view=minimal_view)
+    minimal_database = my_loader.load(source=minimal_household_database, target_class=Database, schema_view=minimal_view)
 
-    assert minimal_instance.id == "http://example.com/minimal_household_schema/1"
+    assert minimal_database.people[0].id == "minhouse:1"
