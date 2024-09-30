bd16f44da5b55d82b12aec9da5a091b5354e1b83
Isaac To <isaac.chun.to@gmail.com>
2024-09-30 12:39:38 -0700

    Add tests for adding class with separate additional slots specification

--- tests/linkml_runtime/test_utils/test_schema_builder.py
+++ tests/linkml_runtime/test_utils/test_schema_builder.py
@@ -1,7 +1,9 @@
+from typing import Optional, List, Union
+
 import pytest
 
 from linkml_runtime.utils.schema_builder import SchemaBuilder
-from linkml_runtime.linkml_model import ClassDefinition
+from linkml_runtime.linkml_model import ClassDefinition, SlotDefinition
 
 
 @pytest.mark.parametrize("replace_if_present", [True, False])
@@ -28,3 +30,51 @@ def test_add_existing_class(replace_if_present):
         with pytest.raises(ValueError, match=f"Class {cls_name} already exists"):
             builder.add_class(cls_repeat)
         assert builder.schema.classes[cls_name].slots == ["name"]
+
+
+@pytest.mark.parametrize(
+    "slots",
+    [
+        None,
+        ["name", "age"],
+        ["name", SlotDefinition(name="age")],
+        [SlotDefinition(name="name"), SlotDefinition(name="age")],
+    ],
+)
+@pytest.mark.parametrize("use_attributes", [True, False])
+def test_add_class_with_slot_additions(
+    slots: Optional[List[Union[str, SlotDefinition]]], use_attributes: bool
+):
+    """
+    Test adding a class with separate additional slots specification
+    """
+    # If `slots` is None, it should be treated as if it were an empty list
+    if slots is None:
+        slots = []
+
+    slot_names = {s if isinstance(s, str) else s.name for s in slots}
+
+    builder = SchemaBuilder()
+
+    cls_name = "Person"
+
+    # Add a class to the schema
+    cls = ClassDefinition(name=cls_name)
+
+    if use_attributes:
+        # === The case where the slots specified should be added to the `attributes`
+        # meta slot of the class ===
+        if any(not isinstance(s, SlotDefinition) for s in slots):
+            with pytest.raises(
+                ValueError,
+                match="If use_attributes=True then slots must be SlotDefinitions",
+            ):
+                builder.add_class(cls, slots=slots, use_attributes=use_attributes)
+        else:
+            builder.add_class(cls, slots=slots, use_attributes=use_attributes)
+            assert builder.schema.classes[cls_name].attributes.keys() == slot_names
+    else:
+        # === The case where the slots specified should be added to the `slots`
+        # meta slot of the schema ===
+        builder.add_class(cls, slots=slots, use_attributes=use_attributes)
+        assert builder.schema.slots.keys() == slot_names
