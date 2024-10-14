e094520665911ef5c12e79a5e3bac9a80d50efca
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-10-14 15:31:43 -0700

    remove kwargs in dataclasses monkeypatch from linkml_runtime

--- linkml/generators/pythongen.py
+++ linkml/generators/pythongen.py
@@ -162,7 +162,6 @@ from datetime import date, datetime, time
 from linkml_runtime.utils.slot import Slot
 from linkml_runtime.utils.metamodelcore import empty_list, empty_dict, bnode
 from linkml_runtime.utils.yamlutils import YAMLRoot, extended_str, extended_float, extended_int
-from linkml_runtime.utils.dataclass_extensions_376 import dataclasses_init_fn_with_kwargs
 from linkml_runtime.utils.formatutils import camelcase, underscore, sfx
 {handlerimport}
 from rdflib import Namespace, URIRef
@@ -171,9 +170,6 @@ from linkml_runtime.utils.curienamespace import CurieNamespace
 
 metamodel_version = "{self.schema.metamodel_version}"
 version = {'"' + self.schema.version + '"' if self.schema.version else None}
-
-# Overwrite dataclasses _init_fn to add **kwargs in __init__
-dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
 {self.gen_namespaces()}
