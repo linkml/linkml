f9dec004a12feaa8f88fb587594b5182fcd3c1c3
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-10-14 17:04:37 -0700

    fix kwarg error type

--- tests/test_utils/test_load_raw_schema.py
+++ tests/test_utils/test_load_raw_schema.py
@@ -115,7 +115,6 @@ def test_load_text(input_path):
             "schema1",
         )
 
-
 @pytest.mark.parametrize(
     "filename",
     [
@@ -130,6 +129,7 @@ def test_representation_errors(filename, input_path):
     fn = input_path(filename)
     try:
         SchemaLoader(fn)
+        assert False, "Expected an exception due to malformed schema"
     except Exception as e:
         # If exception raised, log it and optionally check message
         assert isinstance(e, Exception)
