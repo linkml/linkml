bfcef4f683c19230e205da977c8c28e2cae61108
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-10-14 17:55:26 -0700

    lint, rm unnecessary dataclasses import, and rm from pythongen

--- linkml/reporting/model.py
+++ linkml/reporting/model.py
@@ -6,7 +6,6 @@
 # description: A datamodel for reports on data
 # license: https://creativecommons.org/publicdomain/zero/1.0/
 
-import dataclasses
 from dataclasses import dataclass
 from typing import Any, ClassVar, Optional, Union
 
