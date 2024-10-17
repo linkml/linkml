81981ca14f8603fcb6af2b9d9c46fd4c5639d828
Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>
2024-10-17 11:36:07 +0200

    [tests] add missing network markers
    Signed-off-by: Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>

--- tests/test_base/test_rdf.py
+++ tests/test_base/test_rdf.py
@@ -9,6 +9,7 @@ from tests import LOCAL_MODEL_YAML_NO_META
 
 @pytest.mark.parametrize("format", [".ttl"])
 @pytest.mark.parametrize("model", LOCAL_MODEL_YAML_NO_META)
+@pytest.mark.network
 def test_model_rdf(model, format, snapshot):
     generated = RDFGenerator(model).serialize(context=LOCAL_METAMODEL_LDCONTEXT_FILE)
     output_file = Path(model).with_suffix(format).name
