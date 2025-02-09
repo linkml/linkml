171e3018f55379eb46d7fca9d70047aac9261985
Will Halfpenny <30996474+will-0@users.noreply.github.com>
2025-02-09 23:05:10 +0000

    more robust import checking

--- packages/linkml_runtime/utils/schemaview.py
+++ packages/linkml_runtime/utils/schemaview.py
@@ -18,6 +18,7 @@ from linkml_runtime.utils.pattern import PatternResolver
 from linkml_runtime.linkml_model.meta import *
 from linkml_runtime.exceptions import OrderingError
 from enum import Enum
+from urllib.parse import urlparse
 
 logger = logging.getLogger(__name__)
 
@@ -105,6 +106,14 @@ def is_absolute_path(path: str) -> bool:
         return False
     drive, tail = os.path.splitdrive(norm_path)
     return bool(drive and tail)
+
+def resolve_import(sn: str, i: str) -> str:
+    if is_absolute_path(i):
+        return i
+    if urlparse(i).scheme:
+        return i
+    else:
+        return os.path.normpath(str(Path(sn).parent / i))
 
 
 @dataclass
@@ -291,26 +300,8 @@ class SchemaView(object):
                     # no self imports ;)
                     if i == sn:
                         continue
-
-                    # resolve relative imports relative to the importing schema, rather than the
-                    # origin schema. Imports can be a URI or Curie, and imports from the same
-                    # directory don't require a ./, so if the current (sn) import is a relative
-                    # path, and the target import doesn't have : (as in a curie or a URI)
-                    # we prepend the relative path. This WILL make the key in the `schema_map` not
-                    # equal to the literal text specified in the importing schema, but this is
-                    # essential to sensible deduplication: eg. for
-                    # - main.yaml (imports ./types.yaml, ./subdir/subschema.yaml)
-                    # - types.yaml
-                    # - subdir/subschema.yaml (imports ./types.yaml)
-                    # - subdir/types.yaml
-                    # we should treat the two `types.yaml` as separate schemas from the POV of the
-                    # origin schema.
-                    if sn.startswith('.') and ':' not in i:
-                        if WINDOWS:
-                            # This cannot be simplified. os.path.normpath() must be called before .as_posix()
-                            i = PurePath(os.path.normpath(PurePath(sn).parent / i)).as_posix()
-                        else:
-                            i = os.path.normpath(str(Path(sn).parent / i))
+                    else:
+                        i = resolve_import(sn, i)
                     todo.append(i)
 
             # add item to closure
