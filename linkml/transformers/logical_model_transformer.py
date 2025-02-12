ab4a1ce290aa2b861e433b883c694523d88119d7
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2025-02-12 07:49:04 -0800

    fix codespell errors

--- linkml/transformers/logical_model_transformer.py
+++ linkml/transformers/logical_model_transformer.py
@@ -409,11 +409,11 @@ class LogicalModelTransformer(ModelTransformer):
         target_class_name: ClassDefinitionName,
         ancestors: List[ClassDefinitionName],
     ):
-        anc_classes = [self.schemaview.get_class(anc) for anc in ancestors]
+        anc_classes = [self.schemaview.get_class(ancestor) for ancestor in ancestors]
         attributes: Dict[SlotDefinitionName, SlotDefinition] = {}
-        for anc in anc_classes:
-            top_level_slots = [(s, target_schema.slots[s]) for s in anc.slots]
-            for slot_name, slot_expr in list(anc.attributes.items()) + list(anc.slot_usage.items()) + top_level_slots:
+        for ancestor_class in anc_classes:
+            top_level_slots = [(s, target_schema.slots[s]) for s in ancestor_class.slots]
+            for slot_name, slot_expr in list(ancestor_class.attributes.items()) + list(ancestor_class.slot_usage.items()) + top_level_slots:
                 if slot_name not in attributes:
                     attributes[slot_name] = SlotDefinition(slot_name)
                 sx = attributes[slot_name]
@@ -644,7 +644,7 @@ class LogicalModelTransformer(ModelTransformer):
         if slot_expression.all_of:
             exprs.append(logictools.And(*[self._as_logical_expression(subx) for subx in slot_expression.all_of]))
         if slot_expression.exactly_one_of:
-            # TODO: disjointness
+            # TODO: disjointedness
             exprs.append(logictools.Or(*[self._as_logical_expression(subx) for subx in slot_expression.exactly_one_of]))
         if slot_expression.none_of:
             exprs.append(
