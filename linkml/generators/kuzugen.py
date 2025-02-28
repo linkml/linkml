6080da31bfb502b152d02f35437397d9d5427d13
Kevin Schaper <kevinschaper@gmail.com>
2025-02-27 19:41:07 -0800

    formatting

--- linkml/generators/kuzugen.py
+++ linkml/generators/kuzugen.py
@@ -1,36 +1,38 @@
-from pydantic import BaseModel, model_validator
 from enum import Enum
+
 from jinja2 import Template
+from pydantic import BaseModel, model_validator
 
 ## jinja template for kuzu schema generation
 
+
 class KuzuTableTypeEnum(Enum):
-    NODE = 'NODE'
-    REL = 'REL'
+    NODE = "NODE"
+    REL = "REL"
+
 
 class KuzuFieldTypeEnum(Enum):
-    STRING = 'STRING'
-    INT = 'INT64'
-    FLOAT = 'FLOAT'
-    BOOL = 'BOOLEAN'
-    DATETIME = 'TIMESTAMP'
-    DATE = 'DATE'
-    TIME = 'TIME'
+    STRING = "STRING"
+    INT = "INT64"
+    FLOAT = "FLOAT"
+    BOOL = "BOOLEAN"
+    DATETIME = "TIMESTAMP"
+    DATE = "DATE"
+    TIME = "TIME"
+
 
 class KuzuField(BaseModel):
     name: str
     type: KuzuFieldTypeEnum
 
     class Config:
-        schema_extra = {
-            "required": ["name", "type"]
-        }
+        schema_extra = {"required": ["name", "type"]}
 
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     def check_field_name_is_valid(self):
-        name = self.get('name')
+        name = self.get("name")
         if not name.isidentifier():
-            raise ValueError('Field name {name} is not a valid identifier')
+            raise ValueError("Field name {name} is not a valid identifier")
         return self
 
 
@@ -41,28 +43,27 @@ class KuzuTable(BaseModel):
     primary_key: str
 
     class Config:
-        schema_extra = {
-            "required": ["type", "name", "fields", "primary_key"]
-        }
+        schema_extra = {"required": ["type", "name", "fields", "primary_key"]}
 
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     def check_primary_key_in_fields(self):
-        primary_key = self.get('primary_key')
-        fields = self.get('fields', [])
+        primary_key = self.get("primary_key")
+        fields = self.get("fields", [])
         field_names = [field.name for field in fields]
         if primary_key not in field_names:
-            raise ValueError('primary_key must be one of the fields')
+            raise ValueError("primary_key must be one of the fields")
         return self
-    
-    @model_validator(mode='before')
+
+    @model_validator(mode="before")
     def check_class_name_is_valid(self):
-        name = self.get('name')
+        name = self.get("name")
         if not name.isidentifier():
-            raise ValueError('Table name {name} is not a valid identifier')
-        return self    
+            raise ValueError("Table name {name} is not a valid identifier")
+        return self
 
 
-template = Template("""
+template = Template(
+    """
 {% for table in tables %}
 CREATE {{ table.type.value }} TABLE {{ table.name }} (
     {%- for field in table.fields %}
@@ -71,27 +72,28 @@ CREATE {{ table.type.value }} TABLE {{ table.name }} (
     PRIMARY KEY ({{ table.primary_key }})
 );
 {% endfor %}
-""")
+"""
+)
 
 
 person_table = KuzuTable(
     type=KuzuTableTypeEnum.NODE,
-    name='Person',
+    name="Person",
     fields=[
-        KuzuField(name='name', type=KuzuFieldTypeEnum.STRING),
-        KuzuField(name='age', type=KuzuFieldTypeEnum.INT),
+        KuzuField(name="name", type=KuzuFieldTypeEnum.STRING),
+        KuzuField(name="age", type=KuzuFieldTypeEnum.INT),
     ],
-    primary_key='name'
+    primary_key="name",
 )
 
 knows_table = KuzuTable(
     type=KuzuTableTypeEnum.REL,
-    name='Knows',
+    name="Knows",
     fields=[
-        KuzuField(name='from', type=KuzuFieldTypeEnum.STRING),
-        KuzuField(name='to', type=KuzuFieldTypeEnum.STRING),
+        KuzuField(name="from", type=KuzuFieldTypeEnum.STRING),
+        KuzuField(name="to", type=KuzuFieldTypeEnum.STRING),
     ],
-    primary_key='from'
+    primary_key="from",
 )
 
 tables = [person_table, knows_table]
