da8b9990fb32f8bf45bf74b0719676cf02b15a12
Sujay Patil <sujaysanjeev.patil@gmail.com>
2025-04-21 17:51:50 -0700

    fix ruff import sorting error in linkml/generators/markdowngen.py

--- linkml/generators/markdowngen.py
+++ linkml/generators/markdowngen.py
@@ -18,9 +18,9 @@ from linkml_runtime.utils.formatutils import be, camelcase, underscore
 
 from linkml._version import __version__
 from linkml.generators.yumlgen import YumlGenerator
+from linkml.utils.deprecation import deprecation_warning
 from linkml.utils.generator import Generator, shared_arguments
 from linkml.utils.typereferences import References
-from linkml.utils.deprecation import deprecation_warning
 
 
 @dataclass
