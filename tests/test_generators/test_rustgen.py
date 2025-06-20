19793f7752ebf9c6fef7c6b0830135cd3180c514
Frank Dekervel <kervel@gmail.com>
2025-06-20 14:36:50 +0200

    Add docstrings and slot mode helper

--- tests/test_generators/test_rustgen.py
+++ tests/test_generators/test_rustgen.py
@@ -1,9 +1,43 @@
 import pytest
+from linkml_runtime.utils.schemaview import SchemaView
 
 from linkml.generators.rustgen import RustGenerator
+from linkml.generators.rustgen.rustgen import (
+    SlotContainerMode,
+    SlotInlineMode,
+    determine_slot_mode,
+)
 
 
 @pytest.mark.rustgen
 def test_generate_crate(kitchen_sink_path, temp_dir):
     gen = RustGenerator(kitchen_sink_path, mode="crate", output=temp_dir)
     _ = gen.serialize(force=True)
+
+
+def test_determine_slot_mode(kitchen_sink_path):
+    sv = SchemaView(kitchen_sink_path)
+
+    age_slot = sv.get_slot("age in years")
+    assert determine_slot_mode(age_slot, sv) == (
+        SlotContainerMode.SINGLE_VALUE,
+        SlotInlineMode.PRIMITIVE,
+    )
+
+    aliases_slot = sv.get_slot("aliases")
+    assert determine_slot_mode(aliases_slot, sv) == (
+        SlotContainerMode.LIST,
+        SlotInlineMode.PRIMITIVE,
+    )
+
+    hist_slot = sv.get_slot("has employment history")
+    assert determine_slot_mode(hist_slot, sv) == (
+        SlotContainerMode.LIST,
+        SlotInlineMode.INLINE,
+    )
+
+    employed_slot = sv.get_slot("employed at")
+    assert determine_slot_mode(employed_slot, sv) == (
+        SlotContainerMode.SINGLE_VALUE,
+        SlotInlineMode.REFERENCE,
+    )
