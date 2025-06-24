45f6d0e21f2e051ddfd158fc1e62852eb6a4c288
Frank Dekervel <kervel@gmail.com>
2025-06-24 12:04:48 +0200

    sort imports

--- linkml/generators/rustgen/build.py
+++ linkml/generators/rustgen/build.py
@@ -1,4 +1,5 @@
 from typing import Dict
+
 from linkml.generators.common.build import (
     BuildResult,
     SchemaResult,
@@ -16,10 +17,10 @@ from linkml.generators.rustgen.template import (
     RustCargo,
     RustEnum,
     RustFile,
-    RustTemplateModel,
     RustProperty,
     RustPyProject,
     RustStruct,
+    RustTemplateModel,
     RustTypeAlias,
 )
 
