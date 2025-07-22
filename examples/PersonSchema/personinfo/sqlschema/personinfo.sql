2fe2eff18e80e8475644985bff2b3d619b8ed1ac
Vlad Korolev <vlad@v-lad.org>
2025-07-22 19:03:31 -0400

    Update PersonInfo SQL DLL

--- examples/PersonSchema/personinfo/sqlschema/personinfo.sql
+++ examples/PersonSchema/personinfo/sqlschema/personinfo.sql
@@ -109,23 +109,23 @@ CREATE TABLE "NamedThing" (
 	description TEXT,
 	image TEXT,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_NamedThing_id" ON "NamedThing" (id);
 CREATE TABLE "HasAliases" (
 	id INTEGER NOT NULL,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_HasAliases_id" ON "HasAliases" (id);
 CREATE TABLE "Place" (
 	id TEXT NOT NULL,
 	name TEXT,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_Place_id" ON "Place" (id);
 CREATE TABLE "Address" (
 	id INTEGER NOT NULL,
 	street TEXT,
 	city TEXT,
 	postal_code TEXT,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_Address_id" ON "Address" (id);
 CREATE TABLE "Event" (
 	id INTEGER NOT NULL,
 	started_at_time DATE,
@@ -133,28 +133,28 @@ CREATE TABLE "Event" (
 	duration FLOAT,
 	is_current BOOLEAN,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_Event_id" ON "Event" (id);
 CREATE TABLE "Concept" (
 	id TEXT NOT NULL,
 	name TEXT,
 	description TEXT,
 	image TEXT,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_Concept_id" ON "Concept" (id);
 CREATE TABLE "DiagnosisConcept" (
 	id TEXT NOT NULL,
 	name TEXT,
 	description TEXT,
 	image TEXT,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_DiagnosisConcept_id" ON "DiagnosisConcept" (id);
 CREATE TABLE "ProcedureConcept" (
 	id TEXT NOT NULL,
 	name TEXT,
 	description TEXT,
 	image TEXT,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_ProcedureConcept_id" ON "ProcedureConcept" (id);
 CREATE TABLE "Relationship" (
 	id INTEGER NOT NULL,
 	started_at_time DATE,
@@ -162,11 +162,11 @@ CREATE TABLE "Relationship" (
 	related_to TEXT,
 	type TEXT,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_Relationship_id" ON "Relationship" (id);
 CREATE TABLE "Container" (
 	id INTEGER NOT NULL,
 	PRIMARY KEY (id)
-);
+);CREATE INDEX "ix_Container_id" ON "Container" (id);
 CREATE TABLE "Person" (
 	primary_email TEXT,
 	birth_date TEXT,
@@ -181,7 +181,7 @@ CREATE TABLE "Person" (
 	PRIMARY KEY (id),
 	FOREIGN KEY("Container_id") REFERENCES "Container" (id),
 	FOREIGN KEY(current_address_id) REFERENCES "Address" (id)
-);
+);CREATE INDEX "ix_Person_id" ON "Person" (id);
 CREATE TABLE "Organization" (
 	mission_statement TEXT,
 	founding_date TEXT,
@@ -194,25 +194,25 @@ CREATE TABLE "Organization" (
 	PRIMARY KEY (id),
 	FOREIGN KEY(founding_location) REFERENCES "Place" (id),
 	FOREIGN KEY("Container_id") REFERENCES "Container" (id)
-);
+);CREATE INDEX "ix_Organization_id" ON "Organization" (id);
 CREATE TABLE "WithLocation" (
 	id INTEGER NOT NULL,
 	in_location TEXT,
 	PRIMARY KEY (id),
 	FOREIGN KEY(in_location) REFERENCES "Place" (id)
-);
+);CREATE INDEX "ix_WithLocation_id" ON "WithLocation" (id);
 CREATE TABLE "HasAliases_aliases" (
 	"HasAliases_id" INTEGER,
 	aliases TEXT,
 	PRIMARY KEY ("HasAliases_id", aliases),
 	FOREIGN KEY("HasAliases_id") REFERENCES "HasAliases" (id)
-);
+);CREATE INDEX "ix_HasAliases_aliases_HasAliases_id" ON "HasAliases_aliases" ("HasAliases_id");CREATE INDEX "ix_HasAliases_aliases_aliases" ON "HasAliases_aliases" (aliases);
 CREATE TABLE "Place_aliases" (
 	"Place_id" TEXT,
 	aliases TEXT,
 	PRIMARY KEY ("Place_id", aliases),
 	FOREIGN KEY("Place_id") REFERENCES "Place" (id)
-);
+);CREATE INDEX "ix_Place_aliases_aliases" ON "Place_aliases" (aliases);CREATE INDEX "ix_Place_aliases_Place_id" ON "Place_aliases" ("Place_id");
 CREATE TABLE "FamilialRelationship" (
 	id INTEGER NOT NULL,
 	started_at_time DATE,
@@ -223,7 +223,7 @@ CREATE TABLE "FamilialRelationship" (
 	PRIMARY KEY (id),
 	FOREIGN KEY(related_to) REFERENCES "Person" (id),
 	FOREIGN KEY("Person_id") REFERENCES "Person" (id)
-);
+);CREATE INDEX "ix_FamilialRelationship_id" ON "FamilialRelationship" (id);
 CREATE TABLE "EmploymentEvent" (
 	id INTEGER NOT NULL,
 	employed_at TEXT,
@@ -235,7 +235,7 @@ CREATE TABLE "EmploymentEvent" (
 	PRIMARY KEY (id),
 	FOREIGN KEY(employed_at) REFERENCES "Organization" (id),
 	FOREIGN KEY("Person_id") REFERENCES "Person" (id)
-);
+);CREATE INDEX "ix_EmploymentEvent_id" ON "EmploymentEvent" (id);
 CREATE TABLE "MedicalEvent" (
 	id INTEGER NOT NULL,
 	in_location TEXT,
@@ -251,16 +251,16 @@ CREATE TABLE "MedicalEvent" (
 	FOREIGN KEY("Person_id") REFERENCES "Person" (id),
 	FOREIGN KEY(diagnosis_id) REFERENCES "DiagnosisConcept" (id),
 	FOREIGN KEY(procedure_id) REFERENCES "ProcedureConcept" (id)
-);
+);CREATE INDEX "ix_MedicalEvent_id" ON "MedicalEvent" (id);
 CREATE TABLE "Person_aliases" (
 	"Person_id" TEXT,
 	aliases TEXT,
 	PRIMARY KEY ("Person_id", aliases),
 	FOREIGN KEY("Person_id") REFERENCES "Person" (id)
-);
+);CREATE INDEX "ix_Person_aliases_aliases" ON "Person_aliases" (aliases);CREATE INDEX "ix_Person_aliases_Person_id" ON "Person_aliases" ("Person_id");
 CREATE TABLE "Organization_aliases" (
 	"Organization_id" TEXT,
 	aliases TEXT,
 	PRIMARY KEY ("Organization_id", aliases),
 	FOREIGN KEY("Organization_id") REFERENCES "Organization" (id)
-);
+);CREATE INDEX "ix_Organization_aliases_aliases" ON "Organization_aliases" (aliases);CREATE INDEX "ix_Organization_aliases_Organization_id" ON "Organization_aliases" ("Organization_id");
