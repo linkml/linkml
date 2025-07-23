f4f8d6373bcf89da3695480cc021a102b2a78d83
Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>
2025-07-23 22:02:04 +0200

    tests(generators): update expectations
    Update expectations according previously applied changes.
    
    Flags `metadata`, `emit_metadata` and `head` were buggy, therefore
    multiple fixes needed.
    
    Signed-off-by: Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>

--- tests/test_scripts/__snapshots__/genpython/timepoint.py
+++ tests/test_scripts/__snapshots__/genpython/timepoint.py
@@ -1,7 +1,3 @@
-# Auto generated from timepoint.yaml by pythongen.py version: 0.0.1
-# Generation date: 2000-01-01T00:00:00
-# Schema: timepoint
-#
 # id: http://example.org/tests/timepoint
 # description:
 # license: https://creativecommons.org/publicdomain/zero/1.0/
