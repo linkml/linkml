f55c4a53b2717074da5cb9a2fda1420fda377850
Vlad Korolev <vlad@v-lad.org>
2025-08-04 18:08:02 -0400

    Inline and simplify loader test
    Explicitly add input files for test cases do not read test inputs from output directory

--- tests/linkml_runtime/test_loaders_dumpers/test_loaders_pydantic.py
+++ tests/linkml_runtime/test_loaders_dumpers/test_loaders_pydantic.py
@@ -1,47 +1,14 @@
-import os
-from typing import Union
+from pathlib import Path
 
 import pytest
 from hbreader import FileInfo
 from pydantic import BaseModel
 
-import tests.environment as test_base
 from linkml_runtime.dumpers import yaml_dumper
 from linkml_runtime.loaders import json_loader, yaml_loader
-from linkml_runtime.loaders.loader_root import Loader
-from linkml_runtime.utils.yamlutils import YAMLRoot
 from tests.test_loaders_dumpers.environment import env
 from tests.test_loaders_dumpers.models.books_normalized_pydantic import BookSeries
 from tests.test_loaders_dumpers.models.kitchen_sink_pydantic import Dataset
-
-
-def loader_test(filename: str, model: Union[type[YAMLRoot], type[BaseModel]], loader: Loader) -> None:
-    """
-    Test the various permutations of the supplied loader using the input file 'filename' -- both load and loads
-
-    :param filename: un-pathed file name to load
-    :param model: model to load the file name into
-    :param loader: package that contains 'load' and 'loads' operations
-    """
-    metadata = FileInfo()
-    name, typ = filename.rsplit(".", 1)
-    expected_yaml = env.expected_path("load", name + "_" + typ + ".yaml")
-    if issubclass(model, YAMLRoot):
-        python_obj: YAMLRoot = loader.load(filename, model, metadata=metadata, base_dir=env.indir)
-    elif issubclass(model, BaseModel):
-        python_obj: BaseModel = loader.load(filename, model, metadata=metadata, base_dir=env.indir)
-    else:
-        raise TypeError(f"Unknown target class: {model}")
-    env.eval_single_file(expected_yaml, yaml_dumper.dumps(python_obj))
-
-    # Make sure metadata gets filled out properly
-    rel_path = os.path.abspath(os.path.join(test_base.env.cwd, ".."))
-    assert os.path.normpath("tests/test_loaders_dumpers/input") == os.path.normpath(
-        os.path.relpath(metadata.base_path, rel_path)
-    )
-    assert os.path.normpath(f"tests/test_loaders_dumpers/input/{filename}") == os.path.normpath(
-        os.path.relpath(metadata.source_file, rel_path)
-    )
 
 
 @pytest.mark.parametrize(
@@ -53,5 +20,20 @@ def loader_test(filename: str, model: Union[type[YAMLRoot], type[BaseModel]], lo
         ("kitchen_sink_normalized_inst_01.json", Dataset, json_loader),
     ],
 )
-def test_loader(filename, model, loader):
-    loader_test(filename, model, loader)
+def test_loader_basemodel(filename, model, loader):
+    name = Path(filename).stem
+    type = Path(filename).suffix.lstrip(".")
+    expected_yaml_file = env.input_path(f"{name}_{type}.yaml")
+
+    metadata = FileInfo()
+
+    python_obj: BaseModel = loader.load(filename, model, metadata=metadata, base_dir=env.indir)
+
+    # Load expected output
+    with open(expected_yaml_file) as expf:
+        expected = expf.read()
+
+    got = yaml_dumper.dumps(python_obj)
+    expected_trimmed = expected.replace("\r\n", "\n").strip()
+    got_trimmed = got.replace("\r\n", "\n").strip()
+    assert expected_trimmed == got_trimmed
