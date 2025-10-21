9b72ed9962a2959477bc9c76863b61cf64791ae4
Tim Fliss <t.fliss@gmail.com>
2025-10-21 14:25:58 -0700

    update pandera generator templates

--- linkml/generators/panderagen/linkml_pandera_validator.py
+++ linkml/generators/panderagen/linkml_pandera_validator.py
@@ -1,186 +1,147 @@
-import inspect
-from functools import wraps
+import logging
 
-import pandera
 import polars as pl
 from pandera.api.polars.types import PolarsData
+from pandera.errors import SchemaError
 
-from linkml.generators.panderagen.transforms import (
-    CollectionDictModelTransform,
-    ListDictModelTransform,
-    NestedStructModelTransform,
-    SimpleDictModelTransform,
-)
+from .transforms import CollectionDictLoader, SimpleDictLoader
 
-
-def handle_validation_exceptions(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except pl.exceptions.PanicException:
-            data = args[2] if len(args) > 2 else kwargs.get("data")
-            return data.lazyframe.select(pl.lit(False))
-        except pandera.errors.SchemaError as e:
-            raise e
-        except Exception:
-            data = args[2] if len(args) > 2 else kwargs.get("data")
-            return data.lazyframe.select(pl.lit(False))
-
-    return wrapper
+logger = logging.getLogger(__name__)
 
 
 class LinkmlPanderaValidator:
+    """Recursively calls Pandera validate on nested classes"""
+
     @classmethod
     def get_id_column_name(cls):
+        """
+        _id_name is present in the implementing class
+        """
         return cls._id_name
 
     @classmethod
-    def _simple_dict_fields(cls, column_name):
-        details = cls._INLINE_DETAILS[column_name]  # <-- THESE ARE GOING ON THE OUTER CLASS
-
-        return (details["id"], details["other"])
-
-    @classmethod
-    def _prepare_simple_dict(cls, data: PolarsData):
-        """Returns just the simple dict column transformed to an inlined list form
-
-        note that this method uses collect and iter_rows so is very inefficient
+    def _check_simple_dict(
+        cls,
+        data: PolarsData,
+        nested_cls: type,
+        id_col: str,
+        other_col: str,
+        polars_schema: pl.Schema,
+        polars_schema_dict,
+    ):
+        """The 'simple dict' inlined form is not efficient for dataframe storage.
+        Ideally the dataframe is converted at load time to a schema with
+        only lists of structs. If not, this method transforms at validation time.
         """
         column_name = data.key
-        polars_schema = cls.get_nested_range(column_name).to_schema()
-        (id_column, other_column) = cls._simple_dict_fields(column_name)
 
-        simple_dict_transformer = SimpleDictModelTransform(polars_schema, id_column, other_column)
+        tx = SimpleDictLoader(
+            polars_schema,
+            id_col,
+            other_col,
+            polars_schema_dict[id_col],
+            polars_schema_dict[other_col],
+        )
 
-        one_column_df = data.lazyframe.select(pl.col(column_name)).collect()
+        one_column_lf = data.lazyframe.select(tx.load(column_name)).filter(pl.col(column_name).list.len() > 0)
 
-        list_of_structs = [simple_dict_transformer.transform(e) for [e] in one_column_df.iter_rows()]
+        # may be expensive, consider making it optional
+        actual = one_column_lf.collect_schema()[column_name]
+        expected = pl.List(pl.Struct(polars_schema))
 
-        return pl.DataFrame(pl.Series(list_of_structs).alias(column_name))
+        if actual != expected:
+            raise SchemaError(
+                polars_schema_dict, one_column_lf, f"Schema mismatch for {column_name}: {actual} != {expected}"
+            )
 
-    @classmethod
-    @handle_validation_exceptions
-    def _check_simple_dict(cls, data: PolarsData):
-        """
-        The 'simple dict' format, in which the key serves as a local identifier is not a good match for a PolaRS
-        DataFrame. At present the format is
-        """
-        df = cls._prepare_simple_dict(data)
+        nested_lf = one_column_lf.explode(column_name).unnest(column_name)
 
-        column_name = data.key
+        if isinstance(data.lazyframe, pl.LazyFrame):
+            nested_cls.validate(nested_lf)
+        else:
+            nested_cls.validate(nested_lf.collect(engine="streaming"))
 
-        polars_schema = cls.get_nested_range(column_name).to_schema()
-        simple_transform = SimpleDictModelTransform(polars_schema, *cls._simple_dict_fields(column_name))
-        df = simple_transform.explode_unnest_dataframe(df, column_name)
-
-        nested_cls = cls.get_nested_range(column_name)
-        nested_cls.validate(df)
         return data.lazyframe.select(pl.lit(True))
 
     @classmethod
-    @handle_validation_exceptions
-    def _check_collection_struct(cls, data: PolarsData):
+    def _check_collection_struct(cls, data: PolarsData, nested_cls: type, polars_schema_struct: pl.Struct):
+        """The 'collection dict' inline form is not efficient for dataframe.
+        Ideally the dataframe is converted at load time to a schema with
+        only lists of structs. If not, this method transforms at validation time."""
         column_name = data.key
-        nested_cls = cls.get_nested_range(column_name)
 
-        df = CollectionDictModelTransform.prepare_dataframe(data, column_name, nested_cls)
+        tx = CollectionDictLoader(struct_schema=polars_schema_struct, id_col="id")
 
-        collection_transform = CollectionDictModelTransform(nested_cls.to_schema(), nested_cls.get_id_column_name())
-        df = collection_transform.explode_unnest_dataframe(df, column_name)
+        one_column_lf = data.lazyframe.select(tx.load(column_name))
 
-        nested_cls.validate(df)
+        # may be expensive, consider making it optional
+        actual = one_column_lf.collect_schema()[column_name]
+        expected = pl.List(polars_schema_struct)
+
+        if actual != expected:
+            raise SchemaError(
+                polars_schema_struct, one_column_lf, f"Schema mismatch for {column_name}: {actual} != {expected}"
+            )
+
+        nested_lf = one_column_lf.explode(column_name).unnest(column_name)
+
+        if isinstance(data.lazyframe, pl.LazyFrame):
+            nested_cls.validate(nested_lf)
+        else:
+            nested_cls.validate(nested_lf.collect(engine="streaming"))
+
         return data.lazyframe.select(pl.lit(True))
 
     @classmethod
-    @handle_validation_exceptions
-    def _check_nested_list_struct(cls, data: PolarsData):
+    def _check_nested_list_struct(cls, data: PolarsData, nested_cls: type, polars_schema):
+        """Use explode and unnest operations to pass nested lists to pandera validate"""
+        column_name = data.key
+
+        one_column_lf = data.lazyframe.select(column_name)
+
+        # may be expensive, consider making it optional
+        actual = one_column_lf.collect_schema()[column_name]
+        expected = pl.List(pl.Struct(polars_schema))
+
+        # TODO: form of polars_schema needs to be more regular wrt container
+        if actual != expected:
+            raise SchemaError(
+                polars_schema, one_column_lf, f"Schema mismatch for {column_name}: {actual} != {expected}"
+            )
+
+        # fmt: off
+        nested_lf = (
+            one_column_lf
+            .filter(pl.col(column_name).list.len() > 0)
+            .explode(column_name)
+            .unnest(column_name)
+        )
+        # fmt: on
+        if isinstance(data.lazyframe, pl.LazyFrame):
+            nested_cls.validate(nested_lf)
+        else:
+            nested_cls.validate(nested_lf.collect(engine="streaming"))
+
+        return data.lazyframe.select(pl.lit(True))
+
+    @classmethod
+    def _check_nested_struct(cls, data: PolarsData, nested_cls: type, polars_schema: pl.Schema):
         """Use this in a custom check. Pass the nested model as pandera_model."""
         column_name = data.key
-        nested_cls = cls.get_nested_range(column_name)
 
-        df = ListDictModelTransform.prepare_dataframe(data, column_name, nested_cls)
+        lf = data.lazyframe
 
-        list_transform = ListDictModelTransform(nested_cls.to_schema())
-        df = list_transform.explode_unnest_dataframe(df, column_name, data)
+        # may be expensive, consider making it optional
+        actual = lf.collect_schema()[column_name]
+        expected = pl.Struct(polars_schema)
 
-        nested_cls.validate(df)
+        if actual != expected:
+            raise SchemaError(polars_schema, lf, f"Schema mismatch for {column_name}: {actual} != {expected}")
+
+        nested_lf = lf.select(column_name).unnest(column_name)
+        if isinstance(data.lazyframe, pl.LazyFrame):
+            nested_cls.validate(nested_lf)
+        else:
+            nested_cls.validate(nested_lf.collect(engine="streaming"))
+
         return data.lazyframe.select(pl.lit(True))
-
-    @classmethod
-    @handle_validation_exceptions
-    def _check_nested_struct(cls, data: PolarsData):
-        """Use this in a custom check. Pass the nested model as pandera_model."""
-        column_name = data.key
-        nested_cls = cls.get_nested_range(column_name)
-
-        df = NestedStructModelTransform.prepare_dataframe(data, column_name, nested_cls)
-        nested_transform = NestedStructModelTransform(nested_cls.to_schema())
-        df = nested_transform.explode_unnest_dataframe(df, column_name)
-
-        nested_cls.validate(df)
-        return data.lazyframe.select(pl.lit(True))
-
-    @classmethod
-    def get_nested_range(cls, column_name):
-        """Resolve a nested class range at runtime.
-
-        Nested classes are not stored in the pandera schema,
-        but rather in the _NESTED_RANGES dictionary as strings.
-        """
-        nested_cls_name = cls._NESTED_RANGES[column_name]
-        shared_model_module = inspect.getmodule(cls)
-        nested_cls = getattr(shared_model_module, nested_cls_name)
-
-        return nested_cls
-
-    @classmethod
-    def generate_polars_schema_simple(cls):
-        # This is not nesting or list aware, so needs to be aligned with the other method
-        return pl.Struct({k: v.dtype.type for k, v in cls.to_schema().columns.items()})
-
-    @classmethod
-    def generate_polars_schema(cls, object_to_validate, parser=False) -> dict:
-        """Creates a nested PolaRS schema suitable for loading the object_to_validate.
-        Optional columns that are not present in the data are omitted.
-        This approach is only suitable to enable the test fixtures.
-        """
-        polars_schema = {}
-
-        if isinstance(object_to_validate, list):
-            object_to_validate = object_to_validate[0]
-
-        for column_name, column in cls.to_schema().columns.items():
-            dtype = column.properties["dtype"]
-            required = column.properties["required"]
-
-            if required or column_name in object_to_validate:
-                if dtype.type in [pl.Struct, pl.List]:  # maybe use inline form directly here
-                    inline_form = cls._INLINE_FORM.get(column_name, "not_inline")
-                    if inline_form == "simple_dict":
-                        polars_schema[column_name] = pl.Object  # make this a struct and make the nested non-
-                    elif inline_form == "not_inline":
-                        polars_schema[column_name] = dtype.type
-                    else:
-                        nested_cls = cls.get_nested_range(column_name)
-                        if inline_form == "inlined_dict":
-                            if parser:
-                                nested_schema = nested_cls.generate_polars_schema(
-                                    object_to_validate[column_name], parser
-                                )
-                                polars_schema[column_name] = pl.Struct(nested_schema)
-                            else:
-                                polars_schema[column_name] = pl.Struct
-                        elif inline_form == "inlined_list_dict":
-                            if parser:
-                                nested_schema = nested_cls.generate_polars_schema(
-                                    object_to_validate[column_name], parser
-                                )
-                                polars_schema[column_name] = pl.List(pl.Struct(nested_schema))
-                            else:
-                                # transformed form
-                                polars_schema[column_name] = pl.List
-                else:
-                    polars_schema[column_name] = dtype.type
-
-        return polars_schema
