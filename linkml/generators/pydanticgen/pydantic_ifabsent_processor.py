552bb73c08e301964d517a2ae5e345b5f4ccd7de
Chr1st0p43rR <190404576+Chr1st0p43rR@users.noreply.github.com>
2025-10-21 11:59:14 +0100

    Added pydantic_ifabsent_processor

---
+++ linkml/generators/pydanticgen/pydantic_ifabsent_processor.py
@@ -0,0 +1,14 @@
+from linkml_runtime.linkml_model import (
+    ClassDefinition,
+    EnumDefinitionName,
+    SlotDefinition,
+)
+
+from linkml.generators.python.python_ifabsent_processor import PythonIfAbsentProcessor
+
+class PydanticIfAbsentProcessor(PythonIfAbsentProcessor):
+    
+    def map_enum_default_value(
+        self, enum_name: EnumDefinitionName, permissible_value_name: str, slot: SlotDefinition, cls: ClassDefinition
+    ):
+        return f"{enum_name}.{permissible_value_name}"
