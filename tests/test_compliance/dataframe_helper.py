8447edefd434f4ed4e995929a7a2feecd6a58035
Tim Fliss <t.fliss@gmail.com>
2025-10-21 13:34:03 -0700

    use pandera package generation for compliance tests

--- tests/test_compliance/dataframe_helper.py
+++ tests/test_compliance/dataframe_helper.py
@@ -4,7 +4,6 @@ import sys
 import pytest
 import yaml
 
-from linkml.generators.panderagen.compile_python import compile_python
 from linkml.generators.panderagen.dict_compare import deep_compare_dicts
 
 _MIN_POLARS_VERSION = "1.29.0"
@@ -20,8 +19,6 @@ def apply_skip_list(skip_value: str, skip_list: list[str]) -> None:
 
 
 _SKIP_LIST = [
-    "test_inlined-INLTrue_IALFalse_MVTrue_FKFalse",
-    "test_inlined-INLTrue_IALTrue_MVTrue_FKFalse",
     "test_jsonpointer",
     "test_inlined_unique_keys",
     "test_nested_key",
@@ -41,6 +38,15 @@ _SKIP_LIST = [
 ]
 
 
+class PackageHelper:
+    def __init__(self, compiled_modules):
+        self.compiled_modules = compiled_modules
+
+    def class_from_module(self, module_name, class_name):
+        module = self.compiled_modules[module_name]
+        return getattr(module, class_name)
+
+
 def check_data_pandera(
     schema, output, target_class, object_to_validate, coerced, expected_behavior, valid, polars_only=False
 ):
@@ -48,48 +54,61 @@ def check_data_pandera(
     if sys.version_info < (3, 11):
         pytest.skip("typing.Optional issue for polars generator in python < 3.11")
     pl = pytest.importorskip("polars", minversion=_MIN_POLARS_VERSION, reason="Polars >= 1.0 not installed")
+    from linkml.generators.panderagen.dataframe_generator import DataframeGenerator
+    from linkml.generators.panderagen.panderagen import PANDERA_GROUP, POLARS_GROUP
+
+    if polars_only:
+        group = POLARS_GROUP
+    else:
+        group = PANDERA_GROUP
+
+    schema_yaml = yaml.dump(schema)
+
+    logger.info(f"Validating {target_class} / {coerced}\n\n{schema_yaml}\n\n{output}")
 
     try:
-        mod = compile_python(output)
-        py_cls = getattr(mod, target_class)
-
-        logger.info(
-            f"Validating {target_class} against {object_to_validate} / {coerced} / {expected_behavior} / "
-            f"{valid}\n\n{yaml.dump(schema)}\n\n{output}"
+        compiled_modules = DataframeGenerator.compile_package_from_specification(
+            group, "test_pandera_package", schema_yaml
         )
+        package_helper = PackageHelper(compiled_modules)
+
+        logger.info(f"Behavior: {expected_behavior}")
+        logger.info(f"Valid: {valid}")
+        logger.info(f"Expected: {object_to_validate}")
+
+        strict_schema = polars_only
+        pl_schema = package_helper.class_from_module("panderagen_polars_schema", target_class)
+        dataframe_serialized_form = pl.from_dicts([object_to_validate], schema=pl_schema, strict=strict_schema)
+
+        actual_data = dataframe_serialized_form.to_dicts()[0]
+        same = deep_compare_dicts(object_to_validate, actual_data)
+
+        logger.info(f"Actual: {actual_data}")
+        logger.info(f"Same: {same}")
+
+        if same and not valid:
+            logger.warning("PolaRS schema accepted an invalid object. Note the schema is not a full validator.")
 
         if polars_only:
-            dataframe_to_validate = pl.DataFrame([object_to_validate], schema=py_cls, strict=True)
-            actual_data = dataframe_to_validate.to_dicts()[0]
-            same = deep_compare_dicts(object_to_validate, actual_data)
-            if same:
-                if not valid:
-                    logger.warning("PolaRS schema accepted an invalid object. Note the schema is not a full validator.")
-                else:
-                    logger.info(f"polars round-trip: {object_to_validate} == {actual_data}")
-            else:
-                if valid:
-                    assert same
-
-            # in a future PR this will fall through to pandera when polars_only is False
-            return
+            assert same
         else:
-            dataframe_to_validate = pl.DataFrame([object_to_validate])
+            if not same and not valid:
+                logger.info("PolaRS schema did not load invalid object to validate properly")
 
-        try:
-            schema_name = schema.get("name", "")
-            polars_schema = py_cls.generate_polars_schema(object_to_validate, parser=True)
+            logger.info(dataframe_serialized_form)
 
-            if schema_name.startswith("test_date_types") or schema_name.startswith("test_enum_alias"):
-                dataframe_to_validate = pl.DataFrame(object_to_validate, schema=polars_schema, strict=False)
-            elif dataframe_to_validate.item() is None:
-                dataframe_to_validate = pl.DataFrame(object_to_validate, schema=polars_schema, strict=False)
-        except Exception:
-            pass
+            XForm = package_helper.class_from_module("panderagen_polars_schema_transform", target_class)
+            dataframe_to_validate = XForm().load(dataframe_serialized_form)
 
-        logger.info(dataframe_to_validate)
-        py_cls.validate(dataframe_to_validate, lazy=True)
-        py_cls.validate(dataframe_to_validate)
+            py_cls = package_helper.class_from_module("panderagen_schema_loaded", target_class)
+            py_cls.validate(dataframe_to_validate, lazy=True)
     except Exception as e:
+        logger.info("Actual: EXCEPTION")
+        logger.info("Same: N/A")
+        logger.info(f"Schema Name: {schema['name']}")
         if valid:
+            if not polars_only:
+                logger.info(output)
             raise e
+    finally:
+        DataframeGenerator.cleanup_package("test_pandera_package")
