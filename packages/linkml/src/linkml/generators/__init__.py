16c650849feae91fb0114f149cede8695e8dffe1
FlorianK13 <florian.kotthoff@posteo.de>
2026-02-05 16:39:21 +0100

    Register new generator (#3123)

--- packages/linkml/src/linkml/generators/__init__.py
+++ packages/linkml/src/linkml/generators/__init__.py
@@ -17,6 +17,7 @@ from linkml.generators.shaclgen import ShaclGenerator
 from linkml.generators.shexgen import ShExGenerator
 from linkml.generators.sqlalchemygen import SQLAlchemyGenerator
 from linkml.generators.sqltablegen import SQLTableGenerator
+from linkml.generators.sqlvalidationgen import SQLValidationGenerator
 
 __all__ = [
     "csvgen",
@@ -56,6 +57,7 @@ __all__ = [
     "ShExGenerator",
     "SQLAlchemyGenerator",
     "SQLTableGenerator",
+    "SQLValidationGenerator",
     "RDFGenerator",
 ]
 
