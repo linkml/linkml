3d3eba37a5b62bbd14df007a8cdebff1994f28bd
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-03-27 01:21:04 -0700

    wow how did the lint not catch that

--- packages/linkml/src/linkml/converter/cli.py
+++ packages/linkml/src/linkml/converter/cli.py
@@ -240,7 +240,7 @@ def cli(
         if schema is None:
             raise Exception("--schema must be passed in order to validate. Suppress with --no-validate")
         obj_dict = json_dumper.to_dict(obj)
-        report = run_validation(obj_dict, schema, target_class, raise_=True)
+        report = run_validation(obj_dict, schema, target_class)
         report.raise_for_results()
 
     output_format = _get_format(output, output_format, default="json")
