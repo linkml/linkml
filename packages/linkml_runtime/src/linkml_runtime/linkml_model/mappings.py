5e33f5832ba6d7b5ef2045acddcc125b64b1ceb8
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2026-05-07 12:41:51 +0300

    Update Metamodel files for 1.11.0-rc2

--- packages/linkml_runtime/src/linkml_runtime/linkml_model/mappings.py
+++ packages/linkml_runtime/src/linkml_runtime/linkml_model/mappings.py
@@ -1,5 +1,5 @@
 # Auto generated from mappings.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-02-18T21:04:50
+# Generation date: 2026-05-05T18:49:12
 # Schema: mappings
 #
 # id: https://w3id.org/linkml/mappings
