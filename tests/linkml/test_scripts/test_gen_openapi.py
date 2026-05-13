8b21b697f43cdabb80acf3f431a068291b10461f
Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>
2026-05-13 10:22:05 +0200

    openapigen: add tests for openapi v3.1.0
    Add tests for newly added support for OpenAPI v3.1.0.
    
    Signed-off-by: Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>

--- tests/linkml/test_scripts/test_gen_openapi.py
+++ tests/linkml/test_scripts/test_gen_openapi.py
@@ -3,12 +3,20 @@ from click.testing import CliRunner
 from linkml.generators.openapigen import cli
 from tests.conftest import KITCHEN_SINK_PATH
 
-OPENAPI_TEMPLATE_PATH = str(
+OPENAPI_TEMPLATE_PATH_V303 = str(
     __import__("pathlib").Path(__file__).parent.parent
     / "test_generators"
     / "input"
     / "openapi"
     / "spec-head.openapi.yaml"
+)
+
+OPENAPI_TEMPLATE_PATH_V310 = str(
+    __import__("pathlib").Path(__file__).parent.parent
+    / "test_generators"
+    / "input"
+    / "openapi"
+    / "spec-head-v31.openapi.yaml"
 )
 
 
@@ -18,9 +26,26 @@ def test_help():
     assert "Generate an OpenAPI spec" in result.output
 
 
-def test_valid_call():
+def test_valid_call_v303():
     runner = CliRunner()
-    result = runner.invoke(cli, [KITCHEN_SINK_PATH, "--template", OPENAPI_TEMPLATE_PATH])
+    result = runner.invoke(cli, [KITCHEN_SINK_PATH, "--template", OPENAPI_TEMPLATE_PATH_V303])
+    assert result.exit_code == 0
+    assert "MarriageEvent" in result.output
+    assert "MedicalEvent" in result.output
+
+
+def test_valid_call_v310():
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            KITCHEN_SINK_PATH,
+            "--template",
+            OPENAPI_TEMPLATE_PATH_V310,
+            "--openapi-version",
+            "3.1.0",
+        ],
+    )
     assert result.exit_code == 0
     assert "MarriageEvent" in result.output
     assert "MedicalEvent" in result.output
