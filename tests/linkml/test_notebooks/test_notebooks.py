279bb0e7bda96ccd28563fd986a555b6568a846f
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2026-05-14 14:33:01 -0500

    refactor(tests): stop rewriting source notebooks during test runs

--- tests/linkml/test_notebooks/test_notebooks.py
+++ tests/linkml/test_notebooks/test_notebooks.py
@@ -1,16 +1,12 @@
 import os
 import sys
-from io import StringIO
 
 import nbformat
 import pytest
 from nbconvert.preprocessors import ExecutePreprocessor
 
 from tests.environment import env
-from tests.linkml.test_notebooks.environment import nbenv
-from tests.linkml.utils.filters import nb_filter
 
-FORCE_REWRITE = True
 # Tests moved to root: tests/linkml/test_notebooks, notebooks at root
 NBBASEDIR = os.path.join(env.cwd, "..", "notebooks")
 
@@ -18,13 +14,6 @@ NBBASEDIR = os.path.join(env.cwd, "..", "notebooks")
 @pytest.fixture
 def ep():
     return ExecutePreprocessor(timeout=600)
-
-
-def force_rewrite_comparator(expected: str, actual: str) -> str:
-    msg = nbenv.string_comparator(expected, actual)
-    if not msg and FORCE_REWRITE:
-        msg = "Forced rewrite"
-    return msg
 
 
 @pytest.mark.skipif(
@@ -35,21 +24,8 @@ def force_rewrite_comparator(expected: str, actual: str) -> str:
     "nbname",
     [filename for filename in os.listdir(NBBASEDIR) if not filename.startswith(".") and filename.endswith(".ipynb")],
 )
-def test_redo_notebook(nbname, ep):
-    # The information on how to do this comes from: http://tritemio.github.io/smbits/2016/01/02/execute-notebooks/
+def test_redo_notebook(nbname: str, ep: ExecutePreprocessor) -> None:
+    """Verify the notebook executes end-to-end without raising in any cell."""
     with open(os.path.join(NBBASEDIR, nbname), encoding="utf-8") as nbf:
         nb = nbformat.read(nbf, as_version=4)
     ep.preprocess(nb, dict(metadata=dict(path=NBBASEDIR)))
-
-    outf = StringIO()
-    nbformat.write(nb, outf)
-
-    result = nbenv.eval_single_file(
-        os.path.join(NBBASEDIR, nbname),
-        outf.getvalue(),
-        nb_filter,
-        force_rewrite_comparator,
-    )
-    if result and nbenv.fail_on_error:
-        with open(nbenv.temp_file_path(nbname), "w", encoding="utf-8") as actualf:
-            actualf.write(outf.getvalue())
