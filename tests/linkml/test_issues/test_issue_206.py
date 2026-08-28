9768a5d79b31ddece3b3493ae637cb7cc1f4f98f
N <13322818+noelmcloughlin@users.noreply.github.com>
2026-08-28 21:15:32 +0100

    Update tests/linkml/test_issues/test_issue_206.py
    Co-authored-by: Nico Matentzoglu <nicolas.matentzoglu@gmail.com>

--- tests/linkml/test_issues/test_issue_206.py
+++ tests/linkml/test_issues/test_issue_206.py
@@ -5,4 +5,6 @@ from linkml.utils.schemaloader import SchemaLoader
 
 def test_class_list(input_path):
     """SchemaLoader should be monotonic - metamodel test"""
-    SchemaLoader(input_path("issue_206.yaml")).resolve()
+    schema = SchemaLoader(input_path("issue_206.yaml")).resolve()
+    assert list(schema.classes) == ["Entity"]
+    assert schema.classes["Entity"].slots == ["entity__id", "entity__description"]
